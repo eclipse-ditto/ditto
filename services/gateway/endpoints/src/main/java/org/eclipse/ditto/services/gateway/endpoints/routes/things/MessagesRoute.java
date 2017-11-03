60ac2d4be9f33284f6d4e92674945ba55462bd4c
Cai Yufei <Yufei.Cai@bosch-si.com>
2017-11-03 09:48:20 +0100

    make HTTP headers of message requests case-insensitive
    Signed-off-by: Cai Yufei <Yufei.Cai@bosch-si.com>

--- services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/routes/things/MessagesRoute.java
+++ services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/routes/things/MessagesRoute.java
@@ -264,8 +264,9 @@ final class MessagesRoute extends AbstractRoute {
     private static Map<String, String> getHeadersAsMap(final HttpMessage httpRequest) {
         final Map<String, String> result = new HashMap<>();
         for (final HttpHeader httpHeader : httpRequest.getHeaders()) {
-            if (!BLACKLISTED_HTTP_HEADERS.contains(httpHeader.name().toLowerCase())) {
-                result.put(httpHeader.name(), httpHeader.value());
+            final String lowerCaseHeaderName = httpHeader.name().toLowerCase();
+            if (!BLACKLISTED_HTTP_HEADERS.contains(lowerCaseHeaderName)) {
+                result.put(lowerCaseHeaderName, httpHeader.value());
             }
         }
         return result;
