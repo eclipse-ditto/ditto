99618de7a0369dce6919ef09bba2013a634e1baa
Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>
2017-11-28 19:04:14 +0100

    add missing license header
    Signed-off-by: Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>

--- services/thingsearch/persistence/src/main/java/org/eclipse/ditto/services/thingsearch/persistence/write/impl/MongoThingsSearchSyncPersistence.java
+++ services/thingsearch/persistence/src/main/java/org/eclipse/ditto/services/thingsearch/persistence/write/impl/MongoThingsSearchSyncPersistence.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial contribution
+ */
 package org.eclipse.ditto.services.thingsearch.persistence.write.impl;
 
 import static org.eclipse.ditto.services.thingsearch.persistence.PersistenceConstants.LAST_SUCCESSFUL_SYNC_COLLECTION_NAME;
