15756d71af9fe8b70b74beefbc2c4d25d11a365f
Cai Yufei <Yufei.Cai@bosch-si.com>
2018-01-10 13:23:59 +0100

    set namespaces of search commands to null when no namespace restriction applies
    Signed-off-by: Cai Yufei <Yufei.Cai@bosch-si.com>

--- services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/routes/thingsearch/ThingSearchRoute.java
+++ services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/routes/thingsearch/ThingSearchRoute.java
@@ -17,11 +17,11 @@ import static akka.http.javadsl.server.Directives.path;
 import static akka.http.javadsl.server.Directives.pathEndOrSingleSlash;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.eclipse.ditto.model.base.headers.DittoHeaders;
 import org.eclipse.ditto.services.gateway.endpoints.directives.CustomPathMatchers;
@@ -125,12 +125,17 @@ public final class ThingSearchRoute extends AbstractRoute {
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private static Set<String> calculateNamespaces(final Optional<String> namespacesString) {
-        return namespacesString.filter(s -> !s.isEmpty())
-                .map(s -> s.split(","))
-                .map(Arrays::asList)
-                .map(HashSet::new)
-                .map(n -> (Set<String>) n)
-                .orElse(Collections.emptySet());
+
+        final Function<String, Set<String>> splitAndRemoveEmpty =
+                s -> Arrays.stream(s.split(","))
+                        .filter(segment -> !segment.isEmpty())
+                        .collect(Collectors.toSet());
+
+        // if no namespaces are given explicitly via query parameter,
+        // return null to signify the lack of namespace restriction
+        final Set<String> defaultNamespaces = null;
+
+        return namespacesString.map(splitAndRemoveEmpty).orElse(defaultNamespaces);
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
