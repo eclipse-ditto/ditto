c7ee9d5e901178888480ac0261a13aa6b27b485d
Dominik Guggemos <dominik.guggemos@bosch-si.com>
2018-03-02 09:43:33 +0100

    fix generation of amqp uri once more
    Signed-off-by: Dominik Guggemos <dominik.guggemos@bosch-si.com>

--- services/amqp-bridge/messaging/src/main/java/org/eclipse/ditto/services/amqpbridge/messaging/amqp/AmqpConnectionBasedJmsConnectionFactory.java
+++ services/amqp-bridge/messaging/src/main/java/org/eclipse/ditto/services/amqpbridge/messaging/amqp/AmqpConnectionBasedJmsConnectionFactory.java
@@ -13,8 +13,13 @@ package org.eclipse.ditto.services.amqpbridge.messaging.amqp;
 
 import static org.eclipse.ditto.model.base.common.ConditionChecker.checkNotNull;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -76,20 +81,22 @@ public final class AmqpConnectionBasedJmsConnectionFactory implements JmsConnect
         final int port = amqpConnection.getPort();
         final boolean failoverEnabled = amqpConnection.isFailoverEnabled();
 
-        final String baseUri = formatUri(protocol, hostname, port, failoverEnabled);
+        final String baseUri = formatUri(protocol, hostname, port);
 
-        final List<String> uriParams = new ArrayList<>();
+        final List<String> parameters = new ArrayList<>(getAmqpParameters());
         if (!amqpConnection.isValidateCertificates()) {
-            uriParams.add(getTransportParameters());
+            parameters.addAll(getTransportParameters());
         }
-        uriParams.add(getJmsParameters(id, username, password));
-        uriParams.add(getFailoverParameters());
-        uriParams.add(getAmqpParameters(failoverEnabled));
+        final String nestedUri = baseUri + parameters.stream().collect(Collectors.joining("&", "?", ""));
 
-        final String connectionUri = baseUri + uriParams.stream().collect(Collectors.joining("&", "?", ""));
+        final List<String> globalParameters = new ArrayList<>(getJmsParameters(id, username, password));
+        if (failoverEnabled) {
+            globalParameters.addAll(getFailoverParameters());
+        }
+        final String connectionUri = wrapWithFailOver(nestedUri, failoverEnabled)
+                + globalParameters.stream().collect(Collectors.joining("&", "?", ""));
 
         LOGGER.debug("[{}] URI: {}", id, connectionUri);
-
         @SuppressWarnings("squid:S1149") final Hashtable<Object, Object> env = new Hashtable<>();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
         env.put("connectionfactory." + amqpConnection.getId(), connectionUri);
@@ -97,40 +104,46 @@ public final class AmqpConnectionBasedJmsConnectionFactory implements JmsConnect
         return new InitialContext(env);
     }
 
-    private static String formatUri(final String protocol, final String hostname, final int port,
-            final boolean failoverEnabled) {
+    private static String formatUri(final String protocol, final String hostname, final int port) {
         final String pattern = "{0}://{1}:{2}";
-        final String uri = MessageFormat.format(pattern, protocol, hostname, Integer.toString(port));
-        return failoverEnabled ? wrapWithFailOver(uri) : uri;
+        return MessageFormat.format(pattern, protocol, hostname, Integer.toString(port));
     }
 
     @SuppressWarnings("squid:S2068")
-    private static String getJmsParameters(final String id, final String username, final String password) {
-        return "jms.clientID=" + id +
-                "&jms.username=" + username +
-                "&jms.password=" + password;
+    private static List<String> getJmsParameters(final String id, final String username, final String password) {
+        String encodedId;
+        try {
+            encodedId = URLEncoder.encode(id, StandardCharsets.UTF_8.displayName());
+        } catch (final UnsupportedEncodingException e) {
+            LOGGER.info("Enconding not supported: {}", e.getMessage());
+            //fallback: replace special characters
+            encodedId = id.replaceAll("[^a-zA-Z0-9]+", "");
+        }
+        return Arrays.asList("jms.clientID=" + encodedId,
+                "jms.username=" + username,
+                "jms.password=" + password);
     }
 
-    private static String getAmqpParameters(final boolean nested) {
-        return (nested ? "failover.nested." : "") + "amqp.saslMechanisms=ANONYMOUS,PLAIN";
+    private static List<String> getAmqpParameters() {
+        return Collections.singletonList("amqp.saslMechanisms=PLAIN");
     }
 
-    private static String getTransportParameters() {
-        return "transport.trustAll=true" +
-                "&transport.verifyHost=false";
+    private static List<String> getTransportParameters() {
+        return Arrays.asList("transport.trustAll=true",
+                "transport.verifyHost=false");
     }
 
-    private static String getFailoverParameters() {
-        return "initialReconnectDelay=10s" +
-                "&failover.startupMaxReconnectAttempts=1" + // important, we cannot interrupt connection initiation
-                "&reconnectDelay=1s" +
-                "&maxReconnectDelay=1h" +
-                "&failover.useReconnectBackOff=true" +
-                "&reconnectBackOffMultiplier=1m";
+    private static List<String> getFailoverParameters() {
+        return Arrays.asList("initialReconnectDelay=10s",
+                "failover.startupMaxReconnectAttempts=1", // important, we cannot interrupt connection initiation
+                "reconnectDelay=1s",
+                "maxReconnectDelay=1h",
+                "failover.useReconnectBackOff=true",
+                "reconnectBackOffMultiplier=1m");
     }
 
-    private static String wrapWithFailOver(final String uri) {
-        return MessageFormat.format("failover:({0})", uri);
+    private static String wrapWithFailOver(final String uri, final boolean failoverEnabled) {
+        return failoverEnabled ? MessageFormat.format("failover:({0})", uri) : uri;
     }
 
 }
