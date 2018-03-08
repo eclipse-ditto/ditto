bd04ea6330704a0d024cfc0afba876ba8a12a5e4
Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>
2018-03-08 12:12:25 +0100

    Bootstrap authorization service
    Signed-off-by: Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>

---
+++ services/authorization/starter/src/main/java/org/eclipse/ditto/services/authorization/starter/AuthorizationRootActor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial contribution
+ */
+package org.eclipse.ditto.services.authorization.starter;
+
+import com.typesafe.config.Config;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+
+/**
+ * The root actor of Authorization Service.
+ */
+public class AuthorizationRootActor extends AbstractActor {
+
+    /**
+     * Name of this actor.
+     */
+    public static final String ACTOR_NAME = "authorizationRoot";
+
+    public static final Props props(final Config config, final ActorRef pubSubMediator) {
+
+        return Props.create(AuthorizationRootActor.class, AuthorizationRootActor::new);
+    }
+
+    @Override
+    public Receive createReceive() {
+        // TODO: do something.
+        return ReceiveBuilder.create().build();
+    }
+}
---
+++ services/authorization/starter/src/main/java/org/eclipse/ditto/services/authorization/starter/AuthorizationService.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial contribution
+ */
+package org.eclipse.ditto.services.authorization.starter;
+
+import org.eclipse.ditto.services.base.BaseConfigKeys;
+import org.eclipse.ditto.services.base.DittoService;
+import org.eclipse.ditto.services.base.StatsdMongoDbMetricsStarter;
+import org.eclipse.ditto.utils.jsr305.annotations.AllParametersAndReturnValuesAreNonnullByDefault;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.typesafe.config.Config;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.stream.ActorMaterializer;
+
+/**
+ * The Gateway service for Eclipse Ditto.
+ */
+@AllParametersAndReturnValuesAreNonnullByDefault
+public final class AuthorizationService extends DittoService {
+
+    /**
+     * Name for the Akka actor system of the Gateway service.
+     */
+    static final String SERVICE_NAME = "authorization";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationService.class);
+
+    private static final BaseConfigKeys CONFIG_KEYS = BaseConfigKeys.getBuilder()
+            /* TODO: add config facade
+                    .put(BaseConfigKey.Cluster.MAJORITY_CHECK_ENABLED, ConfigKeys.CLUSTER_MAJORITY_CHECK_ENABLED)
+                    .put(BaseConfigKey.Cluster.MAJORITY_CHECK_DELAY, ConfigKeys.CLUSTER_MAJORITY_CHECK_DELAY)
+                    .put(BaseConfigKey.StatsD.HOSTNAME, ConfigKeys.STATSD_HOSTNAME)
+                    .put(BaseConfigKey.StatsD.PORT, ConfigKeys.STATSD_PORT)
+                    */
+            .build();
+
+    private AuthorizationService() {
+        super(LOGGER, SERVICE_NAME, AuthorizationRootActor.ACTOR_NAME, CONFIG_KEYS);
+    }
+
+    /**
+     * Starts the Gateway service.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
+        final AuthorizationService authorizationService = new AuthorizationService();
+        authorizationService.start();
+    }
+
+    @Override
+    protected void startDevOpsCommandsActor(final ActorSystem actorSystem, final Config config) {
+        // TODO: start DevOpsCommandsActor
+    }
+
+    @Override
+    protected void startStatsdMetricsReporter(final ActorSystem actorSystem, final Config config) {
+        StatsdMongoDbMetricsStarter.newInstance(config, CONFIG_KEYS, actorSystem, SERVICE_NAME, LOGGER).run();
+    }
+
+    @Override
+    protected Props getMainRootActorProps(final Config config, final ActorRef pubSubMediator,
+            final ActorMaterializer materializer) {
+
+        return AuthorizationRootActor.props(config, pubSubMediator);
+    }
+
+}
