6fb07e0ff886d58da7765116a66d0ce9f0e479df
Dominik Guggemos <dominik.guggemos@bosch-si.com>
2018-03-14 11:55:31 +0100

    allow better control how events/responses are published into RabbitMQ
    Signed-off-by: Dominik Guggemos <dominik.guggemos@bosch-si.com>

--- services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/rabbitmq/RabbitMQPublisherActor.java
+++ services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/rabbitmq/RabbitMQPublisherActor.java
@@ -15,13 +15,14 @@ import static org.eclipse.ditto.model.base.common.ConditionChecker.checkNotNull;
 
 import java.nio.ByteBuffer;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
 
+import org.eclipse.ditto.model.base.headers.DittoHeaderDefinition;
 import org.eclipse.ditto.model.connectivity.Connection;
 import org.eclipse.ditto.model.connectivity.ExternalMessage;
-import org.eclipse.ditto.model.base.headers.DittoHeaderDefinition;
 import org.eclipse.ditto.services.connectivity.mapping.MessageMappers;
 import org.eclipse.ditto.services.utils.akka.LogUtil;
 
@@ -38,6 +39,30 @@ import akka.japi.pf.ReceiveBuilder;
 
 /**
  * Responsible for publishing {@link ExternalMessage}s into RabbitMQ / AMQP 0.9.1.
+ * The behaviour how the events and responses are published can be controlled by the configuration settings
+ * {@code eventTarget} and {@code replyTarget}.
+ * <p/>
+ * The {@code eventTarget} from the {@link Connection} is interpreted as follows:
+ * <ul>
+ *     <li>{@code eventTarget} undefined: thing events are not published at all</li>
+ *     <li>{@code eventTarget="target"}: thing events are published to exchange
+ *         {@code target} with default routing key {@code thingEvent}</li>
+ *     <li>{@code eventTarget="target/routingKey"}: thing events are published to exchange
+ *         {@code target} with routing key {@code routingKey}</li>
+ * </ul>
+ * The {@code replyTarget} together with the {@code replyTo} header is interpreted as follows:
+ * <ul>
+ *     <li>{@code replyTarget} and {@code replyTo} header undefined: the response is dropped</li>
+ *     <li>{@code replyTarget="target"} and {@code replyTo} undefined: the response is dropped</li>
+ *     <li>{@code replyTarget} undefined and header {@code replyTo="replyKey"}: the response is sent to the
+ *         default exchange ({@code ""}) with routing key {@code replyKey}</li>
+ *     <li>{@code replyTarget="target"} and header {@code replyTo="replyKey"}, the response is sent to the
+ *         exchange {@code target} with routing key {@code replyKey}</li>
+ *     <li>{@code replyTarget="target/replyKey"} and {@code replyTo} header undefined: the response is sent to the exchange {@code target} with routing key
+ *         {@code replyKey}</li>
+ *     <li>{@code replyTarget="target/replyKey"} and header {@code replyTo="replyKeyFromHeader"}: the response is sent to the exchange {@code target} with routing key
+ *         {@code replyKeyFromHeader}. Note: This means the {@code replyTo} header takes precedence over the configured routing key.</li>
+ * </ul>
  */
 public final class RabbitMQPublisherActor extends AbstractActor {
 
@@ -84,12 +109,17 @@ public final class RabbitMQPublisherActor extends AbstractActor {
                             response.getHeaders().get(DittoHeaderDefinition.CORRELATION_ID.getKey());
                     LogUtil.enhanceLogWithCorrelationId(log, correlationId);
                     log.debug("Received response {} ", response);
-                    final String exchange = connection.getReplyTarget().orElse(DEFAULT_EXCHANGE);
-                    final String routingKey = response.getHeaders().get(ExternalMessage.REPLY_TO_HEADER);
+                    final String exchange =
+                            connection.getReplyTarget().flatMap(this::getExchangeFromTarget).orElse(DEFAULT_EXCHANGE);
+                    final Optional<String> routingKeyFromTarget =
+                            connection.getReplyTarget().flatMap(this::getRoutingKeyFromTarget);
+                    final Optional<String> routingKeyFromHeader =
+                            Optional.ofNullable(response.getHeaders().get(ExternalMessage.REPLY_TO_HEADER));
+                    final String routingKey = routingKeyFromHeader.orElse(routingKeyFromTarget.orElse(null));
                     if (routingKey != null) {
                         publishMessage(exchange, routingKey, response);
                     } else {
-                        log.info("Response dropped due to missing replyTo address.");
+                        log.info("Dropping response, no routingKey defined via config or replyTo header.");
                     }
                 })
                 .match(ExternalMessage.class, ExternalMessage::isEvent, event -> {
@@ -108,6 +138,32 @@ public final class RabbitMQPublisherActor extends AbstractActor {
                     log.debug("Unknown message: {}", m);
                     unhandled(m);
                 }).build();
+    }
+
+    /**
+     * For RabbitMQ connections the target can have the following format: [exchange]/[routingKey].
+     *
+     * @param target the configured target
+     * @return the exchange part of the target, {@link Optional#EMPTY} otherwise.
+     */
+    private Optional<String> getExchangeFromTarget(final String target) {
+        return Optional.of(target.split("/"))
+                .filter(segments -> segments.length > 0)
+                .map(segments -> segments[0])
+                .filter(exchange -> !exchange.isEmpty());
+    }
+
+    /**
+     * For RabbitMQ connections the target can have the following format: [exchange]/[routingKey].
+     *
+     * @param target the configured target
+     * @return the optional routing key part
+     */
+    private Optional<String> getRoutingKeyFromTarget(final String target) {
+        return Optional.of(target.split("/"))
+                .filter(segments -> segments.length == 2)
+                .map(segments -> segments[1])
+                .filter(routingKey -> !routingKey.isEmpty());
     }
 
     private boolean isResponseOrError(final ExternalMessage message) {
