cc2a4b69ef1743a2f0649558815c377d8d93bd7c
Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>
2018-05-16 17:46:11 +0200

    align error message of DittoRejectionHandlerFactory with standard akka message, improve javadoc and comments
    Signed-off-by: Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>

--- services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/utils/DittoRejectionHandlerFactory.java
+++ services/gateway/endpoints/src/main/java/org/eclipse/ditto/services/gateway/endpoints/utils/DittoRejectionHandlerFactory.java
@@ -13,24 +13,28 @@ package org.eclipse.ditto.services.gateway.endpoints.utils;
 
 import static akka.http.javadsl.server.Directives.complete;
 
+import java.text.MessageFormat;
+
 import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.MissingQueryParamRejection;
 import akka.http.javadsl.server.RejectionHandler;
 import akka.http.javadsl.server.Route;
 
 /**
- * Factory to create a custom {@link akka.http.javadsl.server.RejectionHandler}
+ * Factory to create a custom {@link akka.http.javadsl.server.RejectionHandler}.
  */
 public final class DittoRejectionHandlerFactory {
 
-    private static final String MISSING_QUERY_PARAM_TEMPLATE = "Request is missing parameter: '%s'";
+    private static final String MISSING_QUERY_PARAM_TEMPLATE = "Request is missing required query parameter '{0}'";
 
-    private DittoRejectionHandlerFactory() {}
+    private DittoRejectionHandlerFactory() {
+        throw new AssertionError();
+    }
 
     /**
-     * Creates a new instance of {@link akka.http.javadsl.server.RejectionHandler} with custom behaviour
+     * Creates a new instance of {@link akka.http.javadsl.server.RejectionHandler} with custom behaviour.
      *
-     * @return The new instance
+     * @return The new instance.
      */
     public static RejectionHandler createInstance() {
         return RejectionHandler.newBuilder()
@@ -40,7 +44,8 @@ public final class DittoRejectionHandlerFactory {
     }
 
     private static Route handleMissingQueryParam(MissingQueryParamRejection missingQueryParamRejection) {
+        // return status code 400 instead of the akka default 404
         return complete(StatusCodes.BAD_REQUEST,
-                String.format(MISSING_QUERY_PARAM_TEMPLATE, missingQueryParamRejection.parameterName()));
+                MessageFormat.format(MISSING_QUERY_PARAM_TEMPLATE, missingQueryParamRejection.parameterName()));
     }
 }
