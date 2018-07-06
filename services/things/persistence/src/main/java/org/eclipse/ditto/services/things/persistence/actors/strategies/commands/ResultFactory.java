6ede43d97e2d46c954b0972b206f64d37f435283
Johannes Schneider <johannes.schneider@bosch-si.com>
2018-07-06 16:05:20 +0200

    fixed license header
    Signed-off-by: Johannes Schneider <johannes.schneider@bosch-si.com>

--- services/things/persistence/src/main/java/org/eclipse/ditto/services/things/persistence/actors/strategies/commands/ResultFactory.java
+++ services/things/persistence/src/main/java/org/eclipse/ditto/services/things/persistence/actors/strategies/commands/ResultFactory.java
@@ -1,4 +1,14 @@
-/* Copyright (c) 2011-2017 Bosch Software Innovations GmbH, Germany. All rights reserved. */
+/*
+ * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial contribution
+ *
+ */
 package org.eclipse.ditto.services.things.persistence.actors.strategies.commands;
 
 import org.eclipse.ditto.model.base.exceptions.DittoRuntimeException;
