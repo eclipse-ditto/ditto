a74504b7485b9c80650ca40d1f9caf97d4f91733
Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>
2018-09-04 13:46:30 +0200

    revert previous commit (make if-match and if-none-match writable to external headers); this issue can be solved on client-side by using DittoProtocolAdapter with HeaderTranslator#empty()
    Signed-off-by: Daniel Fesenmeyer <daniel.fesenmeyer@bosch-si.com>

--- model/base/src/main/java/org/eclipse/ditto/model/base/headers/DittoHeaderDefinition.java
+++ model/base/src/main/java/org/eclipse/ditto/model/base/headers/DittoHeaderDefinition.java
@@ -122,7 +122,7 @@ public enum DittoHeaderDefinition implements HeaderDefinition {
      * Key: {@code "If-Match"}, Java type: {@link String}.
      * </p>
      */
-    IF_MATCH("If-Match", EntityTagMatchers.class, String.class, true, true),
+    IF_MATCH("If-Match", EntityTagMatchers.class, String.class, true, false),
 
     /**
      * Header definition for "If-None-Match".
@@ -130,7 +130,7 @@ public enum DittoHeaderDefinition implements HeaderDefinition {
      * Key: {@code "If-None-Match"}, Java type: {@link String}.
      * </p>
      */
-    IF_NONE_MATCH("If-None-Match", EntityTagMatchers.class, String.class, true, true);
+    IF_NONE_MATCH("If-None-Match", EntityTagMatchers.class, String.class, true, false);
 
     /**
      * Map to speed up lookup of header definition by key.
