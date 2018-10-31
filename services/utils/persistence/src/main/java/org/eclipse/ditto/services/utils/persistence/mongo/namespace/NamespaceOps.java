28930cb36b2ff7966386cf68a92d0c16ddb30137
Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>
2018-10-31 16:52:45 +0100

    NamespaceOps: impove javadoc
    Signed-off-by: Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>

--- services/utils/persistence/src/main/java/org/eclipse/ditto/services/utils/persistence/mongo/namespace/NamespaceOps.java
+++ services/utils/persistence/src/main/java/org/eclipse/ditto/services/utils/persistence/mongo/namespace/NamespaceOps.java
@@ -20,29 +20,37 @@ import akka.stream.javadsl.Source;
 
 /**
  * Persistence operations on the level of namespaces.
+ * <p>
+ * Currently the only supported operation is 'purge'.
+ * </p>
+ * <p>
+ * Implementations specific to a persistent storage decide what descriptor of a namespace to accept. For example, an
+ * implementation for MongoDB-based event journals may accept a MongoDB-collection-name with a filter. An implementation
+ * for a search index may accept the namespace itself as a string.
+ * </p>
  *
- * @param <S> type of namespace selections.
+ * @param <S> descriptor of a namespace. Implementations decide which type of descriptors to support.
  */
 public interface NamespaceOps<S> {
 
     /**
-     * Purge documents in a namespace from one collection.
+     * Purge a namespace from some persistent storage.
      *
-     * @param selection collection-filter pair to identify documents belonging to a namespace.
+     * @param namespaceDescriptor storage-specific identifier to describe a namespace.
      * @return source of any error during the purge.
      */
-    Source<Optional<Throwable>, NotUsed> purge(S selection);
+    Source<Optional<Throwable>, NotUsed> purge(S namespaceDescriptor);
 
     /**
-     * Purge documents in a namespace from all given collections.
+     * Purge namespaces from some persistent storage.
      *
-     * @param selections collection-filter pairs to identify documents belonging to a namespace.
+     * @param namespaceDescriptors storage-specific identifiers to describe namespaces.
      * @return source of any errors during the purge.
      */
-    default Source<List<Throwable>, NotUsed> purgeAll(final Collection<S> selections) {
-        return Source.from(selections)
+    default Source<List<Throwable>, NotUsed> purgeAll(final Collection<S> namespaceDescriptors) {
+        return Source.from(namespaceDescriptors)
                 .flatMapConcat(this::purge)
-                .grouped(selections.size())
+                .grouped(namespaceDescriptors.size())
                 .map(errors -> errors.stream()
                         .filter(Optional::isPresent)
                         .map(Optional::get)
