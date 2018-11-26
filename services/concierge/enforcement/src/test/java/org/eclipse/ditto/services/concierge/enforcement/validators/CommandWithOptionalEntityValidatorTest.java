e31b8d6dcf24c1a32fc127cf1d96b6aa614d9186
Johannes Schneider <johannes.schneider@bosch-si.com>
2018-11-26 15:45:30 +0100

    added unit test for CommandWithOptionalEntityValidator
    Signed-off-by: Johannes Schneider <johannes.schneider@bosch-si.com>

---
+++ services/concierge/enforcement/src/test/java/org/eclipse/ditto/services/concierge/enforcement/validators/CommandWithOptionalEntityValidatorTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017-2018 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.services.concierge.enforcement.validators;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.eclipse.ditto.json.JsonArray;
+import org.eclipse.ditto.json.JsonObject;
+import org.eclipse.ditto.json.JsonPointer;
+import org.eclipse.ditto.json.JsonValue;
+import org.eclipse.ditto.model.base.exceptions.DittoRuntimeException;
+import org.eclipse.ditto.model.base.headers.DittoHeaders;
+import org.eclipse.ditto.model.base.headers.WithDittoHeaders;
+import org.eclipse.ditto.signals.commands.things.modify.ModifyFeatureProperty;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link org.eclipse.ditto.services.concierge.enforcement.validators.CommandWithOptionalEntityValidator}.
+ */
+public final class CommandWithOptionalEntityValidatorTest {
+
+    private static final String NULL_CHARACTER = "\u0000";
+
+    @Test
+    public void illegalNullCharacterIsInvalidInString() {
+        final JsonValue jsonValue = JsonValue.of(NULL_CHARACTER);
+
+        final WithDittoHeaders withDittoHeaders = createTestCommand(jsonValue);
+
+        assertThatExceptionOfType(DittoRuntimeException.class)
+                .isThrownBy(() -> CommandWithOptionalEntityValidator.getInstance().apply(withDittoHeaders));
+    }
+
+    @Test
+    public void illegalNullCharacterIsInvalidInArray() {
+        final JsonArray jsonArray = JsonArray.newBuilder().add(JsonValue.of(NULL_CHARACTER)).build();
+
+        final WithDittoHeaders withDittoHeaders = createTestCommand(jsonArray);
+
+        assertThatExceptionOfType(DittoRuntimeException.class)
+                .isThrownBy(() -> CommandWithOptionalEntityValidator.getInstance().apply(withDittoHeaders));
+    }
+
+    @Test
+    public void illegalNullCharacterIsInvalidInObject() {
+        final JsonObject jsonObject = JsonObject.newBuilder().set("prop", JsonValue.of(NULL_CHARACTER)).build();
+
+        final WithDittoHeaders withDittoHeaders = createTestCommand(jsonObject);
+
+        assertThatExceptionOfType(DittoRuntimeException.class)
+                .isThrownBy(() -> CommandWithOptionalEntityValidator.getInstance().apply(withDittoHeaders));
+    }
+
+    private WithDittoHeaders createTestCommand(final JsonValue jsonValue) {
+        final String thingId = "com.bosch.iot.things.test:myThing";
+        final String featureId = "myFeature";
+        final JsonPointer propertyJsonPointer = JsonPointer.of("/bumlux");
+        return ModifyFeatureProperty.of(thingId, featureId, propertyJsonPointer, jsonValue, DittoHeaders.empty());
+    }
+
+}
