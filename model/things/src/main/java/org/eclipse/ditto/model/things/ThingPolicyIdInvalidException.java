00893b4161e7f9e7c9ef142873a0de7dec542932
Klem Yannic (INST/ECS1) <Yannic.Klem@bosch-si.com>
2019-01-28 08:00:12 +0100

    make error code all lowercase
    Signed-off-by: Klem Yannic (INST/ECS1) <Yannic.Klem@bosch-si.com>

--- model/things/src/main/java/org/eclipse/ditto/model/things/ThingPolicyIdInvalidException.java
+++ model/things/src/main/java/org/eclipse/ditto/model/things/ThingPolicyIdInvalidException.java
@@ -34,7 +34,7 @@ public final class ThingPolicyIdInvalidException extends DittoRuntimeException i
     /**
      * Error code of this exception.
      */
-    public static final String ERROR_CODE = ERROR_CODE_PREFIX + "policyId.invalid";
+    public static final String ERROR_CODE = ERROR_CODE_PREFIX + "policyid.invalid";
 
     private static final String MESSAGE_TEMPLATE = "Policy ID ''{0}'' is not valid!";
 
