aa2badaf49e25948f44422c7d7605aa02088f103
Thomas Jaeckle <thomas.jaeckle@bosch-si.com>
2019-03-26 09:29:58 +0100

    improved DockerSwarmServiceDiscovery by respecting timeout * fixed lookup * configured bootstrap discovery instead of global akka discovery
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch-si.com>

--- services/base/src/main/java/org/eclipse/ditto/services/base/DockerSwarmServiceDiscovery.java
+++ services/base/src/main/java/org/eclipse/ditto/services/base/DockerSwarmServiceDiscovery.java
@@ -16,14 +16,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import akka.actor.ExtendedActorSystem;
 import akka.discovery.Lookup;
 import akka.discovery.ServiceDiscovery;
 import akka.dispatch.Futures;
+import akka.pattern.Patterns;
 import scala.Option;
 import scala.collection.JavaConverters;
+import scala.concurrent.ExecutionContextExecutor;
 import scala.concurrent.Future;
 import scala.concurrent.duration.FiniteDuration;
 
@@ -48,13 +51,10 @@ public final class DockerSwarmServiceDiscovery extends ServiceDiscovery {
         final String serviceName = lookup.serviceName();
 
         final Optional<String> portName = lookup.getPortName();
-        final Optional<String> protocol = lookup.getProtocol();
-        if (portName.isPresent() || protocol.isPresent()) {
-            throw new IllegalStateException("DockerSwarmServiceDiscovery cannot lookup by port <" +
-                    portName.orElse("") + "> or protocol <" + protocol.orElse("") + ">");
-        }
 
-        return Futures.<Resolved>future(() -> {
+        final ExecutionContextExecutor ec = system.dispatcher();
+
+        final Future<Resolved> resolvedFuture = Futures.<Resolved>future(() -> {
             final InetAddress[] allResolvedHosts;
             try {
                 allResolvedHosts = InetAddress.getAllByName(serviceName);
@@ -65,12 +65,18 @@ public final class DockerSwarmServiceDiscovery extends ServiceDiscovery {
             final List<ResolvedTarget> resolvedTargets = Arrays.stream(allResolvedHosts)
                     .filter(a -> !a.getCanonicalHostName().equals(MY_HOSTNAME))
                     .filter(a -> !a.getHostName().equals(MY_HOSTNAME))
-                    .map(a -> new ResolvedTarget(a.getCanonicalHostName(), Option.empty(), Option.empty()))
+                    .map(a -> new ResolvedTarget(a.getCanonicalHostName(), Option.apply(portName.orElse(null)),
+                            Option.apply(a)))
                     .collect(Collectors.toList());
 
             system.log().warning("[DockerSwarmServiceDiscovery] Resolved via InetAddress: {}", resolvedTargets);
 
             return new Resolved(serviceName, JavaConverters.asScalaBuffer(resolvedTargets).toList());
-        }, system.dispatcher());
+        }, ec);
+
+        final Future<Resolved> timeoutFuture = Patterns.<Resolved>after(resolveTimeout, system.scheduler(), ec,
+                Futures.<Resolved>failed(new TimeoutException("Lookup timed out after " + resolveTimeout)));
+
+        return Futures.<Resolved>firstCompletedOf(Arrays.asList(resolvedFuture, timeoutFuture), ec);
     }
 }
