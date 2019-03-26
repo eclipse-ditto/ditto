91835747dbb6a996d12f2381e43f3bf89a438e58
Thomas Jaeckle <thomas.jaeckle@bosch-si.com>
2019-03-26 08:24:54 +0100

    improved DockerSwarmServiceDiscovery - avoid throwing exception when lookup did not find the service
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch-si.com>

--- services/base/src/main/java/org/eclipse/ditto/services/base/DockerSwarmServiceDiscovery.java
+++ services/base/src/main/java/org/eclipse/ditto/services/base/DockerSwarmServiceDiscovery.java
@@ -13,7 +13,9 @@ package org.eclipse.ditto.services.base;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import akka.actor.ExtendedActorSystem;
@@ -32,9 +34,10 @@ public final class DockerSwarmServiceDiscovery extends ServiceDiscovery {
 
     private final ExtendedActorSystem system;
 
+    private static final String MY_HOSTNAME = System.getenv("HOSTNAME");
+
     /**
      *
-     * @param system
      */
     public DockerSwarmServiceDiscovery(final ExtendedActorSystem system) {
         this.system = system;
@@ -44,18 +47,30 @@ public final class DockerSwarmServiceDiscovery extends ServiceDiscovery {
     public Future<Resolved> lookup(final Lookup lookup, final FiniteDuration resolveTimeout) {
         final String serviceName = lookup.serviceName();
 
-        final InetAddress[] allResolvedHosts;
-        try {
-            allResolvedHosts = InetAddress.getAllByName(serviceName);
-        } catch (final UnknownHostException e) {
-            throw new IllegalStateException("Could not resolve service name " + serviceName, e);
+        final Optional<String> portName = lookup.getPortName();
+        final Optional<String> protocol = lookup.getProtocol();
+        if (portName.isPresent() || protocol.isPresent()) {
+            throw new IllegalStateException("DockerSwarmServiceDiscovery cannot lookup by port <" +
+                    portName.orElse("") + "> or protocol <" + protocol.orElse("") + ">");
         }
-        final List<ResolvedTarget> resolvedTargets = Arrays.stream(allResolvedHosts)
-                .map(a -> new ResolvedTarget(a.getCanonicalHostName(), Option.empty(), Option.empty()))
-                .collect(Collectors.toList());
 
-        final Resolved resolved = new Resolved(serviceName, JavaConverters.asScalaBuffer(resolvedTargets).toList());
-        system.log().warning("[DockerSwarmServiceDiscovery] Resolved via InetAddress: {}", resolved);
-        return Futures.<Resolved>successful(resolved);
+        return Futures.<Resolved>future(() -> {
+            final InetAddress[] allResolvedHosts;
+            try {
+                allResolvedHosts = InetAddress.getAllByName(serviceName);
+            } catch (final UnknownHostException e) {
+                return new Resolved(serviceName,
+                        JavaConverters.<ResolvedTarget>asScalaBuffer(Collections.emptyList()).toList());
+            }
+            final List<ResolvedTarget> resolvedTargets = Arrays.stream(allResolvedHosts)
+                    .filter(a -> !a.getCanonicalHostName().equals(MY_HOSTNAME))
+                    .filter(a -> !a.getHostName().equals(MY_HOSTNAME))
+                    .map(a -> new ResolvedTarget(a.getCanonicalHostName(), Option.empty(), Option.empty()))
+                    .collect(Collectors.toList());
+
+            system.log().warning("[DockerSwarmServiceDiscovery] Resolved via InetAddress: {}", resolvedTargets);
+
+            return new Resolved(serviceName, JavaConverters.asScalaBuffer(resolvedTargets).toList());
+        }, system.dispatcher());
     }
 }
