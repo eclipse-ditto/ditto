4428b36e9c13b173ee85510ff2ec849a3e11f793
Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>
2019-04-09 12:23:25 +0200

    Issue #348: add missing import
    Signed-off-by: Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>

--- services/utils/devops/src/main/java/org/eclipse/ditto/services/utils/devops/DevOpsCommandsActor.java
+++ services/utils/devops/src/main/java/org/eclipse/ditto/services/utils/devops/DevOpsCommandsActor.java
@@ -31,6 +31,7 @@ import org.eclipse.ditto.json.JsonFactory;
 import org.eclipse.ditto.json.JsonObject;
 import org.eclipse.ditto.model.base.common.HttpStatusCode;
 import org.eclipse.ditto.model.base.exceptions.DittoRuntimeException;
+import org.eclipse.ditto.model.base.headers.DittoHeaders;
 import org.eclipse.ditto.model.base.json.Jsonifiable;
 import org.eclipse.ditto.model.devops.LoggerConfig;
 import org.eclipse.ditto.model.devops.LoggingFacade;
