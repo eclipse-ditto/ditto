7dc02e7a1bb981111b91c3d5f03dc466829e7384
Thomas Jaeckle <thomas.jaeckle@bosch-si.com>
2019-04-12 11:48:38 +0200

    fixed license header
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch-si.com>

--- protocol-adapter/src/test/java/org/eclipse/ditto/protocoladapter/DittoEncodingHelperTest.java
+++ protocol-adapter/src/test/java/org/eclipse/ditto/protocoladapter/DittoEncodingHelperTest.java
@@ -1,4 +1,15 @@
-/* Copyright (c) 2011-2017 Bosch Software Innovations GmbH, Germany. All rights reserved. */
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.ditto.protocoladapter;
 
 import static org.assertj.core.api.Assertions.assertThat;
