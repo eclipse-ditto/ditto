5f65cdba49d8e48eab5f64c909bd7d572e658bd6
Juergen Fickel <juergen.fickel@bosch-si.com>
2019-04-23 08:20:23 +0200

    Issue #350: Added unit test for `RawConfigSupplier`.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch-si.com>

---
+++ services/base/src/test/java/org/eclipse/ditto/services/base/config/raw/RawConfigSupplierTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017-2018 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.services.base.config.raw;
+
+import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
+import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link RawConfigSupplier}.
+ */
+public final class RawConfigSupplierTest {
+
+    @Test
+    public void assertImmutability() {
+        assertInstancesOf(RawConfigSupplier.class, areImmutable());
+    }
+
+}
