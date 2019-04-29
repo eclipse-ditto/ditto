820f2adbecbfbadbd189831364debd9d53e378d7
Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>
2019-04-29 15:43:34 +0200

    add missing annotation to PurgeEntitiesResponse
    Signed-off-by: Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>

--- signals/commands/common/src/main/java/org/eclipse/ditto/signals/commands/common/purge/PurgeEntitiesResponse.java
+++ signals/commands/common/src/main/java/org/eclipse/ditto/signals/commands/common/purge/PurgeEntitiesResponse.java
@@ -28,6 +28,7 @@ import org.eclipse.ditto.json.JsonObjectBuilder;
 import org.eclipse.ditto.model.base.common.HttpStatusCode;
 import org.eclipse.ditto.model.base.headers.DittoHeaders;
 import org.eclipse.ditto.model.base.json.FieldType;
+import org.eclipse.ditto.model.base.json.JsonParsableCommandResponse;
 import org.eclipse.ditto.model.base.json.JsonSchemaVersion;
 import org.eclipse.ditto.signals.commands.base.CommandResponseJsonDeserializer;
 import org.eclipse.ditto.signals.commands.common.CommonCommandResponse;
@@ -36,6 +37,7 @@ import org.eclipse.ditto.signals.commands.common.CommonCommandResponse;
  * Response to {@link PurgeEntities}.
  */
 @Immutable
+@JsonParsableCommandResponse(type = PurgeEntitiesResponse.TYPE)
 public final class PurgeEntitiesResponse extends CommonCommandResponse<PurgeEntitiesResponse> {
 
     /**
