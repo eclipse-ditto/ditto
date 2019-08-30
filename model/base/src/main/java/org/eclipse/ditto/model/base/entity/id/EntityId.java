c1ba2c96cabaded9303ef79232e727b1ed4b472f
Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>
2019-08-30 08:56:25 +0200

    Fix wrong indentation
    * Some classes were indented with a single space. I guess this was a
      mistake caused by my wrong licence header template.
    
    Signed-off-by: Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>

--- model/base/src/main/java/org/eclipse/ditto/model/base/entity/id/EntityId.java
+++ model/base/src/main/java/org/eclipse/ditto/model/base/entity/id/EntityId.java
@@ -1,56 +1,56 @@
- /*
-  * Copyright (c) 2019 Contributors to the Eclipse Foundation
-  *
-  * See the NOTICE file(s) distributed with this work for additional
-  * information regarding copyright ownership.
-  *
-  * This program and the accompanying materials are made available under the
-  * terms of the Eclipse Public License 2.0 which is available at
-  * http://www.eclipse.org/legal/epl-2.0
-  *
-  * SPDX-License-Identifier: EPL-2.0
-  */
- package org.eclipse.ditto.model.base.entity.id;
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.model.base.entity.id;
 
- import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.Immutable;
 
- /**
-  * Java representation of an Entity ID.
-  */
- @Immutable
- public interface EntityId extends CharSequence, Comparable<EntityId> {
+/**
+ * Java representation of an Entity ID.
+ */
+@Immutable
+public interface EntityId extends CharSequence, Comparable<EntityId> {
 
-     @Override
-     default int length() {
-         return toString().length();
-     }
+    @Override
+    default int length() {
+        return toString().length();
+    }
 
-     @Override
-     default char charAt(final int index) {
-         return toString().charAt(index);
-     }
+    @Override
+    default char charAt(final int index) {
+        return toString().charAt(index);
+    }
 
-     @Override
-     default CharSequence subSequence(final int start, final int end) {
-         return toString().subSequence(start, end);
-     }
+    @Override
+    default CharSequence subSequence(final int start, final int end) {
+        return toString().subSequence(start, end);
+    }
 
-     /**
-      * Compares the entity IDs based on their String representation.
-      *
-      * @param o the other entity ID.
-      * @return a negative integer, zero, or a positive integer as this object
-      * is less than, equal to, or greater than the specified object.
-      */
-     @Override
-     default int compareTo(final EntityId o) {
-         return toString().compareTo(o.toString());
-     }
+    /**
+     * Compares the entity IDs based on their String representation.
+     *
+     * @param o the other entity ID.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     */
+    @Override
+    default int compareTo(final EntityId o) {
+        return toString().compareTo(o.toString());
+    }
 
-     /**
-      * Checks whether this ID acts as dummy ID. Dummy IDs should not be used as ID for any entity.
-      *
-      * @return true if this ID is a dummy ID, otherwise false.
-      */
-     boolean isDummy();
- }
+    /**
+     * Checks whether this ID acts as dummy ID. Dummy IDs should not be used as ID for any entity.
+     *
+     * @return true if this ID is a dummy ID, otherwise false.
+     */
+    boolean isDummy();
+}
--- model/base/src/main/java/org/eclipse/ditto/model/base/entity/id/NamespacedEntityId.java
+++ model/base/src/main/java/org/eclipse/ditto/model/base/entity/id/NamespacedEntityId.java
@@ -1,41 +1,41 @@
- /*
-  * Copyright (c) 2019 Contributors to the Eclipse Foundation
-  *
-  * See the NOTICE file(s) distributed with this work for additional
-  * information regarding copyright ownership.
-  *
-  * This program and the accompanying materials are made available under the
-  * terms of the Eclipse Public License 2.0 which is available at
-  * http://www.eclipse.org/legal/epl-2.0
-  *
-  * SPDX-License-Identifier: EPL-2.0
-  */
- package org.eclipse.ditto.model.base.entity.id;
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.model.base.entity.id;
 
- import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.Immutable;
 
- /**
-  * Interface for all entity IDs that contain a namespace in their string representation.
-  * Every implementation of this interface needs to ensure that name and namespace are valid according to
-  * {@link org.eclipse.ditto.model.base.entity.id.RegexPatterns#ENTITY_NAME_REGEX} and
-  * {@link org.eclipse.ditto.model.base.entity.id.RegexPatterns#NAMESPACE_REGEX}.
-  * Every implementation must ensure immutability.
-  */
- @Immutable
- public interface NamespacedEntityId extends EntityId {
+/**
+ * Interface for all entity IDs that contain a namespace in their string representation.
+ * Every implementation of this interface needs to ensure that name and namespace are valid according to
+ * {@link org.eclipse.ditto.model.base.entity.id.RegexPatterns#ENTITY_NAME_REGEX} and
+ * {@link org.eclipse.ditto.model.base.entity.id.RegexPatterns#NAMESPACE_REGEX}.
+ * Every implementation must ensure immutability.
+ */
+@Immutable
+public interface NamespacedEntityId extends EntityId {
 
-     /**
-      * Gets the name part of this entity ID.
-      *
-      * @return the name if the entity.
-      */
-     String getName();
+    /**
+     * Gets the name part of this entity ID.
+     *
+     * @return the name if the entity.
+     */
+    String getName();
 
-     /**
-      * Gets the namespace part of this entity ID.
-      *
-      * @return the namespace o the entity.
-      */
-     String getNamespace();
+    /**
+     * Gets the namespace part of this entity ID.
+     *
+     * @return the namespace o the entity.
+     */
+    String getNamespace();
 
- }
+}
