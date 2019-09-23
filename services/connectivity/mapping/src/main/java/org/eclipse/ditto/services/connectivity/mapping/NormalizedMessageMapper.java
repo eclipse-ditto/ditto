ac51390b33759fd6c006ebb2c3a8101d11118bb2
Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>
2019-09-23 15:27:33 +0200

    Add normalized message mapper.
    Signed-off-by: Cai Yufei (INST/ECS1) <yufei.cai@bosch-si.com>

---
+++ services/connectivity/mapping/src/main/java/org/eclipse/ditto/services/connectivity/mapping/NormalizedMessageMapper.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.services.connectivity.mapping;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.eclipse.ditto.json.JsonCollectors;
+import org.eclipse.ditto.json.JsonFactory;
+import org.eclipse.ditto.json.JsonFieldDefinition;
+import org.eclipse.ditto.json.JsonKey;
+import org.eclipse.ditto.json.JsonObject;
+import org.eclipse.ditto.json.JsonObjectBuilder;
+import org.eclipse.ditto.json.JsonPointer;
+import org.eclipse.ditto.json.JsonValue;
+import org.eclipse.ditto.model.base.headers.DittoHeaders;
+import org.eclipse.ditto.model.things.Thing;
+import org.eclipse.ditto.model.things.ThingId;
+import org.eclipse.ditto.protocoladapter.Adaptable;
+import org.eclipse.ditto.protocoladapter.JsonifiableAdaptable;
+import org.eclipse.ditto.protocoladapter.Payload;
+import org.eclipse.ditto.protocoladapter.TopicPath;
+import org.eclipse.ditto.services.models.connectivity.ExternalMessage;
+import org.eclipse.ditto.services.models.connectivity.ExternalMessageFactory;
+
+/**
+ * A message mapper implementation for normalized changes.
+ * Create- and modify-events are mapped to nested sparse JSON.
+ * All other signals and incoming messages are dropped.
+ */
+public final class NormalizedMessageMapper implements MessageMapper {
+
+    private static final JsonFieldDefinition<String> THING_ID = Thing.JsonFields.ID;
+    private static final JsonFieldDefinition<String> MODIFIED = Thing.JsonFields.MODIFIED;
+    private static final JsonFieldDefinition<JsonObject> ABRIDGED_ORIGINAL_MESSAGE =
+            JsonFactory.newJsonObjectFieldDefinition("_context");
+
+    /**
+     * Constructs a new {@code MessageMapper} object.
+     * This constructor is required as the the instance is created via reflection.
+     */
+    public NormalizedMessageMapper() {}
+
+    @Override
+    public void configure(final MappingConfig mappingConfig, final MessageMapperConfiguration configuration) {
+        // no op
+    }
+
+    @Override
+    public Optional<String> getContentType() {
+        // No content type loads this mapper automatically.
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Adaptable> map(final ExternalMessage message) {
+        // All incoming messages are dropped.
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<ExternalMessage> map(final Adaptable adaptable) {
+        final TopicPath topicPath = adaptable.getTopicPath();
+        return isCreatedOrModifiedThingEvent(topicPath)
+                ? Optional.of(flattenAsThingChange(adaptable))
+                : Optional.empty();
+    }
+
+    private ExternalMessage flattenAsThingChange(final Adaptable adaptable) {
+        final TopicPath topicPath = adaptable.getTopicPath();
+        final Payload payload = adaptable.getPayload();
+        final JsonPointer path = JsonPointer.of(payload.getPath());
+        final Optional<JsonValue> payloadValue = payload.getValue();
+        final JsonObjectBuilder builder = JsonObject.newBuilder();
+
+        builder.set(THING_ID, ThingId.of(topicPath.getNamespace(), topicPath.getId()).toString());
+        if (path.isEmpty() && payloadValue.isPresent()) {
+            final JsonValue value = payloadValue.get();
+            if (value.isObject()) {
+                value.asObject().forEach(builder::set);
+            } else {
+                // this is impossible; the adaptable should be the protocol message of an event.
+                throw new IllegalStateException("Got adaptable with empty path and non-object value: " + adaptable);
+            }
+        } else {
+            payloadValue.ifPresent(jsonValue -> builder.set(path, jsonValue));
+        }
+        payload.getTimestamp().ifPresent(timestamp -> builder.set(MODIFIED, timestamp.toString()));
+        builder.set(ABRIDGED_ORIGINAL_MESSAGE, abridgeMessage(adaptable));
+
+
+        final Map<String, String> headers = getHeaders(adaptable);
+
+        return ExternalMessageFactory.newExternalMessageBuilder(headers)
+                .withTopicPath(adaptable.getTopicPath())
+                .withText(builder.build().toString())
+                .build();
+    }
+
+    private Map<String, String> getHeaders(final Adaptable adaptable) {
+        // TODO: add config to filter headers
+        return new LinkedHashMap<>(adaptable.getHeaders().orElse(DittoHeaders.empty()));
+    }
+
+    private static JsonObject abridgeMessage(final Adaptable adaptable) {
+        final Payload payload = adaptable.getPayload();
+        final JsonObjectBuilder builder = JsonObject.newBuilder();
+        // add fields of an event protocol message excluding "value" and "status"
+        builder.set(JsonifiableAdaptable.JsonFields.TOPIC, adaptable.getTopicPath().getPath());
+        builder.set(Payload.JsonFields.PATH, payload.getPath().toString());
+        payload.getRevision().ifPresent(revision -> builder.set(Payload.JsonFields.REVISION, revision));
+        payload.getTimestamp().ifPresent(timestamp -> builder.set(Payload.JsonFields.TIMESTAMP, timestamp.toString()));
+        payload.getFields().ifPresent(fields -> builder.set(Payload.JsonFields.FIELDS, fields.toString()));
+        builder.set(JsonifiableAdaptable.JsonFields.HEADERS,
+                dittoHeadersToJson(adaptable.getHeaders().orElse(DittoHeaders.empty())));
+        return builder.build();
+    }
+
+    private static JsonObject dittoHeadersToJson(final DittoHeaders dittoHeaders) {
+        return dittoHeaders.entrySet()
+                .stream()
+                .map(entry -> JsonFactory.newField(JsonKey.of(entry.getKey()), JsonFactory.newValue(entry.getValue())))
+                .collect(JsonCollectors.fieldsToObject());
+    }
+
+    private static boolean isCreatedOrModifiedThingEvent(final TopicPath topicPath) {
+        final Optional<TopicPath.Action> action = topicPath.getAction();
+        if (topicPath.getCriterion() == TopicPath.Criterion.EVENTS &&
+                topicPath.getGroup() == TopicPath.Group.THINGS &&
+                action.isPresent()) {
+            switch (action.get()) {
+                case CREATED:
+                case MODIFIED:
+                    return true;
+            }
+        }
+        return false;
+    }
+
+}
