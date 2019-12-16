d05f009a44c077a4c91a2f16fa67a21b52368fff
Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>
2019-12-16 16:42:17 +0100

    Handle thing ID on create same as for http put requests
    * If ID is present in thing JSON and in topic poth, they must be equal
    * If ID is not present in thing JSON, use the ID from topic path.
    
    Signed-off-by: Klem Yannic (INST/ECS1) <yannic.klem@bosch-si.com>

--- protocol-adapter/src/main/java/org/eclipse/ditto/protocoladapter/AbstractAdapter.java
+++ protocol-adapter/src/main/java/org/eclipse/ditto/protocoladapter/AbstractAdapter.java
@@ -17,6 +17,7 @@ import static org.eclipse.ditto.model.base.common.ConditionChecker.checkNotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -46,6 +47,7 @@ import org.eclipse.ditto.model.things.Thing;
 import org.eclipse.ditto.model.things.ThingDefinition;
 import org.eclipse.ditto.model.things.ThingId;
 import org.eclipse.ditto.model.things.ThingsModelFactory;
+import org.eclipse.ditto.signals.commands.things.exceptions.ThingIdNotExplicitlySettableException;
 
 /**
  * Abstract implementation of {@link Adapter} to provide common functionality.
@@ -94,10 +96,24 @@ abstract class AbstractAdapter<T extends Jsonifiable> implements Adapter<T> {
     }
 
     protected static Thing thingFrom(final Adaptable adaptable) {
-        return adaptable.getPayload().getValue()
+        final Thing thing = adaptable.getPayload().getValue()
                 .map(JsonValue::asObject)
                 .map(ThingsModelFactory::newThing)
                 .orElseThrow(() -> JsonParseException.newBuilder().build());
+
+        final Optional<ThingId> thingIdOptional = thing.getEntityId();
+        final ThingId thingIdFromTopic = thingIdFrom(adaptable);
+
+        if (thingIdOptional.isPresent()) {
+            if (!thingIdOptional.get().equals(thingIdFromTopic)) {
+                throw ThingIdNotExplicitlySettableException.forWebSocket().build();
+            }
+            return thing;
+        } else {
+            return thing.toBuilder()
+                    .setId(thingIdFromTopic)
+                    .build();
+        }
     }
 
     protected static JsonArray thingsArrayFrom(final Adaptable adaptable) {
