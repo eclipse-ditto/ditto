20509a63808b4fe0a363cbb2d8b8e8e7fd5537a5
Juergen Fickel <juergen.fickel@bosch-si.com>
2020-01-21 15:57:53 +0100

    Issue eclipse/ditto#561: Fixed typos in Javadoc comments.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch-si.com>

--- services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/MappingResultHandler.java
+++ services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/MappingResultHandler.java
@@ -12,13 +12,12 @@
  */
 package org.eclipse.ditto.services.connectivity.messaging;
 
-import java.util.function.BinaryOperator;
-
 /**
- * Interface providing methods for the different outcomes of a mapping - dropped, error and success. As a message may
- * either be the input for multiple mappings or a single mapping my produce multiple results, the defined methods may
- * be called multiple times for a single processed message. Multiple mappers are independent, the result of mapper A
- * does not influence the result of mapper B. E.g. mapper A fails but mapper B is successful is a valid case.
+ * Interface providing methods for the different outcomes of a mapping - dropped, error and success.
+ * As a message may either be the input for multiple mappings or a single mapping may produce multiple results, the
+ * defined methods may be called multiple times for a single processed message.
+ * Multiple mappers are independent, the result of mapper A does not influence the result of mapper B.
+ * E.g. mapper A fails but mapper B is successful is a valid case.
  *
  * @param <T> the type of successfully mapped messages
  * @param <R> the type of results of the handler
@@ -28,7 +27,7 @@ interface MappingResultHandler<T, R> {
     /**
      * Is called when the mapping was successful.
      *
-     * @param mappedMessage the successfully mapped message
+     * @param mappedMessage the successfully mapped message.
      * @return result for a mapped message.
      */
     R onMessageMapped(T mappedMessage);
@@ -43,7 +42,7 @@ interface MappingResultHandler<T, R> {
     /**
      * Is called when the mapping failed.
      *
-     * @param ex the exception that was thrown by the mapper
+     * @param ex the exception that was thrown by the mapper.
      */
     R onException(Exception ex);
 
@@ -57,9 +56,10 @@ interface MappingResultHandler<T, R> {
     R combineResults(R left, R right);
 
     /**
-     * Create an empty result. Should be the left and right identiy of the
+     * Create an empty result. Should be the left and right identity of {@link #combineResults(Object, Object)}.
      *
      * @return the empty result.
      */
     R emptyResult();
+
 }
