9a8aabddac0ffa356f6d8399f585e0c08fa14b6a
Yufei Cai <yufei.cai@bosch-si.com>
2020-02-19 14:57:42 +0100

    Add outgoing signals for search subscriptions.
    Signed-off-by: Yufei Cai <yufei.cai@bosch-si.com>

---
+++ signals/events/thingsearch/src/main/java/org/eclipse/ditto/signals/events/thingsearch/SubscriptionEvent.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.ditto.signals.events.thingsearch;
+
+import org.eclipse.ditto.model.base.headers.DittoHeaders;
+import org.eclipse.ditto.signals.events.base.Event;
+
+/**
+ * Interface for all outgoing messages related to a subscription for search results..
+ *
+ * @param <T> the type of the implementing class.
+ * @since 1.1.0
+ */
+public interface SubscriptionEvent<T extends SubscriptionEvent<T>> extends Event<T> {
+
+    /**
+     * Resource type of subscription events.
+     */
+    String RESOURCE_TYPE = "thing-search.subscription";
+
+    /**
+     * Type Prefix of Thing events.
+     */
+    String TYPE_PREFIX = RESOURCE_TYPE + "." + TYPE_QUALIFIER + ":";
+
+    @Override
+    T setDittoHeaders(DittoHeaders dittoHeaders);
+
+    @Override
+    default String getResourceType() {
+        return RESOURCE_TYPE;
+    }
+
+}
