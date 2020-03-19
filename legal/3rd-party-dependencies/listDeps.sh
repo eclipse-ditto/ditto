de2616b4caa3894a043f865ae6f835e6192b9b40
Thomas Jaeckle <thomas.jaeckle@bosch.io>
2020-03-19 14:00:34 +0100

    updated legal stuff: * fixed scripts to gather used dependencies * updated used dependencies * updated NOTICE-THIRD-PARTY.md
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch.io>

--- legal/3rd-party-dependencies/listDeps.sh
+++ legal/3rd-party-dependencies/listDeps.sh
@@ -10,15 +10,22 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 cd ../../
+
 mvn dependency:list -DexcludeGroupIds=org.eclipse.ditto,rubygems -Dsort=true -DoutputFile=dependencies.txt
-find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:compile'|sort|uniq > legal/3rd-party-dependencies//compile.txt
-find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:runtime'|sort|uniq > legal/3rd-party-dependencies//runtime.txt
-find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:test'|sort|uniq > legal/3rd-party-dependencies//test.txt
-find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:provided'|sort|uniq > legal/3rd-party-dependencies//provided.txt
+find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:compile'| tr -d '[:blank:]'| sed -e 's/(optional)//' -e 's/:compile.*/:compile/'|sort|uniq > legal/3rd-party-dependencies//compile.txt
+find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:runtime'| tr -d '[:blank:]'| sed -e 's/(optional)//' -e 's/:runtime.*/:runtime/'|sort|uniq > legal/3rd-party-dependencies//runtime.txt
+find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:test'| tr -d '[:blank:]'| sed -e 's/(optional)//' -e 's/:test.*/:test/'|sort|uniq > legal/3rd-party-dependencies//test.txt
+find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:provided'| tr -d '[:blank:]'| sed -e 's/(optional)//' -e 's/:provided.*/:provided/'|sort|uniq > legal/3rd-party-dependencies//provided.txt
+
+# Cleanup temp files
 find . -name dependencies.txt|while read i; do rm $i;done
+
 cd legal/3rd-party-dependencies/
+
+# exclude compile dependencies from provided.txt + sort + remove duplicates
 cat compile.txt|cut -d':' -f1-4|while read i; do grep -h $i provided.txt;done|sort|uniq|while read x; do sed -i.bak -e s/$x// provided.txt ;done
 sed -i.bak '/^[[:space:]]*$/d' provided.txt
+# exclude compile+provided dependencies from test.txt + sort + remove duplicates
 cat compile.txt provided.txt|cut -d':' -f1-4|while read i; do grep -h $i test.txt;done|sort|uniq|while read x; do sed -i.bak -e s/$x// test.txt ;done
 sed -i.bak '/^[[:space:]]*$/d' test.txt
 rm *.bak
