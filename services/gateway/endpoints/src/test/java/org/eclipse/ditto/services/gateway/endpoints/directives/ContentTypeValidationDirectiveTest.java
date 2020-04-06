3dd1d826a87412f75c78b508608d81f9466894e0
Joel Bartelheimer <joel.bartelheimer@bosch.io>
2020-04-06 11:19:26 +0200

    Add test for the composed directive
    Signed-off-by: Joel Bartelheimer <joel.bartelheimer@bosch.io>

--- services/gateway/endpoints/src/test/java/org/eclipse/ditto/services/gateway/endpoints/directives/ContentTypeValidationDirectiveTest.java
+++ services/gateway/endpoints/src/test/java/org/eclipse/ditto/services/gateway/endpoints/directives/ContentTypeValidationDirectiveTest.java
@@ -22,6 +22,7 @@ import java.util.function.Supplier;
 import org.eclipse.ditto.model.base.headers.DittoHeaders;
 import org.junit.Test;
 
+import akka.actor.ActorSystem;
 import akka.http.javadsl.model.ContentType;
 import akka.http.javadsl.model.ContentTypes;
 import akka.http.javadsl.model.HttpRequest;
@@ -29,14 +30,27 @@ import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.testkit.JUnitRouteTest;
 import akka.http.javadsl.testkit.TestRouteResult;
+import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
-import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 
 public class ContentTypeValidationDirectiveTest extends JUnitRouteTest {
 
     private final Supplier<Route> COMPLETE_OK = () -> complete(StatusCodes.OK);
+
+    private final Materializer MAT = ActorMaterializer.create(ActorSystem.create("QuickStart"));
+    private final String PAYLOAD_STRING = "something";
+    private final Function<Source<ByteString, Object>, Route> OK_WHEN_PAYLOAD_MATCHES = payload -> {
+        final String payloadAsString = payload
+                .map(ByteString::utf8String)
+                .runReduce(String::concat, MAT)
+                .toCompletableFuture()
+                .join();
+
+        return payloadAsString.equals(PAYLOAD_STRING) ? complete(StatusCodes.OK) :
+                complete(StatusCodes.BAD_REQUEST);
+    };
 
     @Test
     public void testValidContentType() {
@@ -49,7 +63,6 @@ public class ContentTypeValidationDirectiveTest extends JUnitRouteTest {
                 testRoute(extractRequestContext(
                         ctx -> ensureValidContentType(List.of(type), ctx, dittoHeaders, COMPLETE_OK)))
                         .run(HttpRequest.PUT("someUrl").withEntity(type, "something".getBytes()));
-
 
         // Assert
         result.assertStatusCode(StatusCodes.OK);
@@ -66,8 +79,7 @@ public class ContentTypeValidationDirectiveTest extends JUnitRouteTest {
         final TestRouteResult result =
                 testRoute(extractRequestContext(
                         ctx -> ensureValidContentType(List.of(type), ctx, dittoHeaders, COMPLETE_OK)))
-                        .run(HttpRequest.PUT("someUrl").withEntity(differentType,"something".getBytes()));
-
+                        .run(HttpRequest.PUT("someUrl").withEntity(differentType, "something".getBytes()));
 
         // Assert
         result.assertStatusCode(StatusCodes.UNSUPPORTED_MEDIA_TYPE);
@@ -85,9 +97,25 @@ public class ContentTypeValidationDirectiveTest extends JUnitRouteTest {
                         ctx -> ensureValidContentType(List.of(type), ctx, dittoHeaders, COMPLETE_OK)))
                         .run(HttpRequest.PUT("someUrl"));
 
-
         // Assert
         result.assertStatusCode(StatusCodes.UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    @Test
+    public void testComposedDirective() {
+        // Arrange
+        DittoHeaders dittoHeaders = DittoHeaders.empty();
+        final ContentType type = ContentTypes.APPLICATION_JSON;
+
+        // Act
+        final TestRouteResult result =
+                testRoute(extractRequestContext(
+                        ctx -> ensureContentTypeAndExtractDataBytes(List.of(type), ctx, dittoHeaders,
+                                OK_WHEN_PAYLOAD_MATCHES)))
+                        .run(HttpRequest.PUT("someUrl").withEntity(type, PAYLOAD_STRING.getBytes()));
+
+        // Assert
+        result.assertStatusCode(StatusCodes.OK);
     }
 
 
