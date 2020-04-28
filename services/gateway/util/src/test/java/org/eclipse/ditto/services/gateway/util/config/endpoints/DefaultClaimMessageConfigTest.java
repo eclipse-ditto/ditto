da28600cd2f848f6d38425a1ac9fe94a9c9da111
Stefan Maute <stefan.maute@bosch.io>
2020-04-28 07:37:00 +0200

    moved remaining config tests and test config to new package;
    Signed-off-by: Stefan Maute <stefan.maute@bosch.io>

--- services/gateway/util/src/test/java/org/eclipse/ditto/services/gateway/util/config/endpoints/DefaultClaimMessageConfigTest.java
+++ services/gateway/util/src/test/java/org/eclipse/ditto/services/gateway/util/config/endpoints/DefaultClaimMessageConfigTest.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.ditto.services.gateway.endpoints.config;
+package org.eclipse.ditto.services.gateway.util.config.endpoints;
 
 import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
 import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
@@ -18,8 +18,7 @@ import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable
 import java.time.Duration;
 
 import org.assertj.core.api.JUnitSoftAssertions;
-import org.eclipse.ditto.services.gateway.util.config.endpoints.DefaultCommandConfig;
-import org.eclipse.ditto.services.gateway.util.config.endpoints.MessageConfig;
+import org.eclipse.ditto.services.gateway.util.config.endpoints.DefaultClaimMessageConfig;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,54 +29,54 @@ import com.typesafe.config.ConfigFactory;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 /**
- * Unit test for {@link org.eclipse.ditto.services.gateway.util.config.endpoints.DefaultCommandConfig}.
+ * Unit test for {@link org.eclipse.ditto.services.gateway.util.config.endpoints.DefaultClaimMessageConfig}.
  */
-public final class DefaultCommandConfigTest {
+public final class DefaultClaimMessageConfigTest {
 
-    private static Config commandTestConfig;
+    private static Config claimMessageTestConfig;
 
     @Rule
     public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
 
     @BeforeClass
     public static void initTestFixture() {
-        commandTestConfig = ConfigFactory.load("command-test");
+        claimMessageTestConfig = ConfigFactory.load("claim-message-test");
     }
 
     @Test
     public void assertImmutability() {
-        assertInstancesOf(DefaultCommandConfig.class, areImmutable());
+        assertInstancesOf(DefaultClaimMessageConfig.class, areImmutable());
     }
 
     @Test
     public void testHashCodeAndEquals() {
-        EqualsVerifier.forClass(DefaultCommandConfig.class)
+        EqualsVerifier.forClass(DefaultClaimMessageConfig.class)
                 .usingGetClass()
                 .verify();
     }
 
     @Test
     public void underTestReturnsDefaultValuesIfBaseConfigWasEmpty() {
-        final DefaultCommandConfig underTest = DefaultCommandConfig.of(ConfigFactory.empty());
+        final DefaultClaimMessageConfig underTest = DefaultClaimMessageConfig.of(ConfigFactory.empty());
 
         softly.assertThat(underTest.getDefaultTimeout())
-                .as(MessageConfig.MessageConfigValue.DEFAULT_TIMEOUT.getConfigPath())
+                .as(DefaultClaimMessageConfig.MessageConfigValue.DEFAULT_TIMEOUT.getConfigPath())
                 .isEqualTo(Duration.ofSeconds(10L));
         softly.assertThat(underTest.getMaxTimeout())
-                .as(MessageConfig.MessageConfigValue.MAX_TIMEOUT.getConfigPath())
+                .as(DefaultClaimMessageConfig.MessageConfigValue.MAX_TIMEOUT.getConfigPath())
                 .isEqualTo(Duration.ofSeconds(60L));
     }
 
     @Test
     public void underTestReturnsValuesOfBaseConfig() {
-        final DefaultCommandConfig underTest = DefaultCommandConfig.of(commandTestConfig);
+        final DefaultClaimMessageConfig underTest = DefaultClaimMessageConfig.of(claimMessageTestConfig);
 
         softly.assertThat(underTest.getDefaultTimeout())
-                .as(MessageConfig.MessageConfigValue.DEFAULT_TIMEOUT.getConfigPath())
-                .isEqualTo(Duration.ofSeconds(33L));
+                .as(DefaultClaimMessageConfig.MessageConfigValue.DEFAULT_TIMEOUT.getConfigPath())
+                .isEqualTo(Duration.ofSeconds(42L));
         softly.assertThat(underTest.getMaxTimeout())
-                .as(MessageConfig.MessageConfigValue.MAX_TIMEOUT.getConfigPath())
-                .isEqualTo(Duration.ofSeconds(55L));
+                .as(DefaultClaimMessageConfig.MessageConfigValue.MAX_TIMEOUT.getConfigPath())
+                .isEqualTo(Duration.ofSeconds(23L));
     }
 
 }
--- services/gateway/util/src/test/java/org/eclipse/ditto/services/gateway/util/config/endpoints/GatewayHttpConfigTest.java
+++ services/gateway/util/src/test/java/org/eclipse/ditto/services/gateway/util/config/endpoints/GatewayHttpConfigTest.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.ditto.services.gateway.endpoints.config;
+package org.eclipse.ditto.services.gateway.util.config.endpoints;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
