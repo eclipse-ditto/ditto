b1b36c3a6bcf3d688dcc583e71d4c24b649f93d4
Juergen Fickel <juergen.fickel@bosch.io>
2020-06-22 10:39:23 +0200

    Widened parameters from `CompletableFuture` to `Future` as this would suffice here.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- services/gateway/security/src/main/java/org/eclipse/ditto/services/gateway/security/authentication/AuthenticationResultWaiter.java
+++ services/gateway/security/src/main/java/org/eclipse/ditto/services/gateway/security/authentication/AuthenticationResultWaiter.java
@@ -13,8 +13,8 @@
 package org.eclipse.ditto.services.gateway.security.authentication;
 
 import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
@@ -39,12 +39,11 @@ public final class AuthenticationResultWaiter<R extends AuthenticationResult>
 
     private static final Duration AWAIT_AUTH_TIMEOUT = Duration.ofSeconds(5L);
 
-    private final CompletableFuture<R> authenticationResultFuture;
+    private final Future<R> authenticationResultFuture;
     private final DittoHeaders dittoHeaders;
     private final Duration awaitAuthTimeout;
 
-    private AuthenticationResultWaiter(final CompletableFuture<R> authenticationResultFuture,
-            final DittoHeaders dittoHeaders,
+    private AuthenticationResultWaiter(final Future<R> authenticationResultFuture, final DittoHeaders dittoHeaders,
             final Duration awaitAuthTimeout) {
 
         this.authenticationResultFuture = authenticationResultFuture;
@@ -61,7 +60,7 @@ public final class AuthenticationResultWaiter<R extends AuthenticationResult>
      * @return the created instance.
      */
     public static <R extends AuthenticationResult> AuthenticationResultWaiter<R> of(
-            final CompletableFuture<R> authenticationResultFuture, final DittoHeaders dittoHeaders) {
+            final Future<R> authenticationResultFuture, final DittoHeaders dittoHeaders) {
 
         return new AuthenticationResultWaiter<>(authenticationResultFuture, dittoHeaders, AWAIT_AUTH_TIMEOUT);
     }
