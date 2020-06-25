2c518473f8af9f80d550fe9984680226cd122e30
David Schwilk <david.schwilk@bosch.io>
2020-06-25 11:38:46 +0200

    Issue #661: Changed JSON serialization of acknowledgement payload in HttpPublisherActor
    Signed-off-by: David Schwilk <david.schwilk@bosch.io>

--- services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/httppush/HttpPublisherActor.java
+++ services/connectivity/messaging/src/main/java/org/eclipse/ditto/services/connectivity/messaging/httppush/HttpPublisherActor.java
@@ -26,6 +26,7 @@ import java.util.function.BiFunction;
 
 import javax.annotation.Nullable;
 
+import org.eclipse.ditto.json.JsonObject;
 import org.eclipse.ditto.json.JsonValue;
 import org.eclipse.ditto.model.base.acks.AcknowledgementLabel;
 import org.eclipse.ditto.model.base.common.HttpStatusCode;
@@ -48,6 +49,7 @@ import org.eclipse.ditto.signals.base.Signal;
 
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.model.ContentTypes;
 import akka.http.javadsl.model.HttpCharset;
 import akka.http.javadsl.model.HttpEntities;
 import akka.http.javadsl.model.HttpEntity;
@@ -248,7 +250,7 @@ final class HttpPublisherActor extends BasePublisherActor<HttpPublishTarget> {
         } else {
             final HttpStatusCode statusCode = statusOptional.orElseThrow();
             return getResponseBody(response, ackSizeQuota, materializer).thenApply(body ->
-                    Acknowledgement.of(label, entityIdWithType, statusCode, dittoHeaders, JsonValue.of(body))
+                    Acknowledgement.of(label, entityIdWithType, statusCode, dittoHeaders, body)
             );
         }
     }
@@ -267,8 +269,9 @@ final class HttpPublisherActor extends BasePublisherActor<HttpPublishTarget> {
         return message.getBytePayload().map(ByteBuffer::array).orElse(new byte[0]);
     }
 
-    private static CompletionStage<String> getResponseBody(final HttpResponse response, final int maxBytes,
+    private static CompletionStage<JsonValue> getResponseBody(final HttpResponse response, final int maxBytes,
             final ActorMaterializer materializer) {
+
         return response.entity()
                 .withSizeLimit(maxBytes)
                 .toStrict(READ_BODY_TIMEOUT_MS, materializer)
@@ -276,7 +279,13 @@ final class HttpPublisherActor extends BasePublisherActor<HttpPublishTarget> {
                     final Charset charset = strictEntity.getContentType().getCharsetOption()
                             .map(HttpCharset::nioCharset)
                             .orElse(StandardCharsets.UTF_8);
-                    return new String(strictEntity.getData().toArray(), charset);
+                    final String bodyString = new String(strictEntity.getData().toArray(), charset);
+                    if (strictEntity.getContentType() == ContentTypes.APPLICATION_JSON) {
+                        try {return JsonObject.of(bodyString);} catch (Exception e) {
+                            return JsonValue.of(bodyString);
+                        }
+                    }
+                    return JsonValue.of(bodyString);
                 });
     }
 
