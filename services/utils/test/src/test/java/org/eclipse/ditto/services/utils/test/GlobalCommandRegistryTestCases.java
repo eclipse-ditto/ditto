1da033c6c0af5d994ded139f7ee4980bc1b24bfa
Yufei Cai <yufei.cai@bosch.io>
2020-09-24 17:13:56 +0200

    Issue #792: stop testing DummyQueryCommand.
    Signed-off-by: Yufei Cai <yufei.cai@bosch.io>

--- services/utils/test/src/test/java/org/eclipse/ditto/services/utils/test/GlobalCommandRegistryTestCases.java
+++ services/utils/test/src/test/java/org/eclipse/ditto/services/utils/test/GlobalCommandRegistryTestCases.java
@@ -23,11 +23,11 @@ import java.util.stream.StreamSupport;
 
 import javax.annotation.concurrent.Immutable;
 
-import org.eclipse.ditto.signals.commands.base.Command;
 import org.atteo.classindex.ClassIndex;
 import org.eclipse.ditto.json.JsonObject;
 import org.eclipse.ditto.model.base.headers.DittoHeaders;
 import org.eclipse.ditto.model.base.json.JsonParsableCommand;
+import org.eclipse.ditto.signals.commands.base.Command;
 import org.junit.Before;
 import org.junit.Test;
 import org.mutabilitydetector.internal.javassist.Modifier;
@@ -56,7 +56,7 @@ public abstract class GlobalCommandRegistryTestCases {
      * @param sample a class that is annotated with {@link JsonParsableCommand}.
      * @param furtherSamples further classes that are annotated with {@link JsonParsableCommand}.
      */
-    protected GlobalCommandRegistryTestCases(final Class<?> sample, final Class<?> ... furtherSamples) {
+    protected GlobalCommandRegistryTestCases(final Class<?> sample, final Class<?>... furtherSamples) {
         samples = new ArrayList<>(1 + furtherSamples.length);
         samples.add(sample);
         Collections.addAll(samples, furtherSamples);
@@ -64,9 +64,10 @@ public abstract class GlobalCommandRegistryTestCases {
 
     @Before
     public void setup() {
-        jsonParsableCommands = StreamSupport.stream(ClassIndex.getAnnotated(JsonParsableCommand.class).spliterator(), true)
-                .filter(c -> !c.getSimpleName().equals("TestCommand"))
-                .collect(Collectors.toList());
+        jsonParsableCommands =
+                StreamSupport.stream(ClassIndex.getAnnotated(JsonParsableCommand.class).spliterator(), true)
+                        .filter(c -> !c.getSimpleName().equals("TestCommand"))
+                        .collect(Collectors.toList());
     }
 
     /**
@@ -112,7 +113,8 @@ public abstract class GlobalCommandRegistryTestCases {
         StreamSupport.stream(ClassIndex.getSubclasses(Command.class).spliterator(), true)
                 .filter(c -> {
                     final int m = c.getModifiers();
-                    return !(Modifier.isAbstract(m) || Modifier.isInterface(m) || isExcluded(c));
+                    return !(Modifier.isAbstract(m) || Modifier.isInterface(m) || Modifier.isPrivate(m) ||
+                            isExcluded(c));
                 })
                 .forEach(c -> assertThat(c.isAnnotationPresent(JsonParsableCommand.class))
                         .as("Check that '%s' is annotated with JsonParsableCommand.", c.getName())
