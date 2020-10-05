8d3fa1d5e74a6b3e4b25d6df5cf20a6eaac443cb
Yannic Klem <yannic.klem@bosch.io>
2020-10-05 10:30:41 +0200

    consider a live response with a non timeout status code to be successful
    * This allows to send live responses with error status codes without
      rejecting the initial message in the consumer actor.
    
    Signed-off-by: Yannic Klem <yannic.klem@bosch.io>

--- signals/acks/base/src/main/java/org/eclipse/ditto/signals/acks/base/ImmutableAcknowledgement.java
+++ signals/acks/base/src/main/java/org/eclipse/ditto/signals/acks/base/ImmutableAcknowledgement.java
@@ -27,6 +27,7 @@ import org.eclipse.ditto.json.JsonObject;
 import org.eclipse.ditto.json.JsonObjectBuilder;
 import org.eclipse.ditto.json.JsonValue;
 import org.eclipse.ditto.model.base.acks.AcknowledgementLabel;
+import org.eclipse.ditto.model.base.acks.DittoAcknowledgementLabel;
 import org.eclipse.ditto.model.base.common.HttpStatusCode;
 import org.eclipse.ditto.model.base.entity.id.EntityIdWithType;
 import org.eclipse.ditto.model.base.entity.type.EntityType;
@@ -93,6 +94,13 @@ final class ImmutableAcknowledgement<T extends EntityIdWithType> implements Ackn
 
     @Override
     public boolean isSuccess() {
+        if (DittoAcknowledgementLabel.LIVE_RESPONSE.equals(label)) {
+            /*
+             *Consider live responses only as failed acknowledgement when the response timed out.
+             * Otherwise it would not be possible to respond with an error status code to live messages.
+             */
+            return !isTimeout();
+        }
         return statusCode.isSuccess();
     }
 
