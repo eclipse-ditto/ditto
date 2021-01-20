3a2fcb1ef493399ff5dac2471792ad362a15e867
Joel Bartelheimer <joel.bartelheimer@bosch.io>
2021-01-20 12:26:27 +0100

    add mailbox-size metric for each actorclass and grouped the config values for that custom mailbox.
    Signed-off-by: Joel Bartelheimer <joel.bartelheimer@bosch.io>

--- services/utils/akka/src/main/java/org/eclipse/ditto/services/utils/akka/mailbox/LoggingMailboxType.java
+++ services/utils/akka/src/main/java/org/eclipse/ditto/services/utils/akka/mailbox/LoggingMailboxType.java
@@ -10,15 +10,21 @@
   *
   * SPDX-License-Identifier: EPL-2.0
   */
- package org.eclipse.ditto.services.utils.akka.actors.mailbox;
+ package org.eclipse.ditto.services.utils.akka.mailbox;
 
+ import java.util.Optional;
  import java.util.Queue;
  import java.util.concurrent.ConcurrentLinkedQueue;
  import java.util.concurrent.atomic.AtomicInteger;
 
+ import org.eclipse.ditto.services.utils.metrics.DittoMetrics;
+ import org.eclipse.ditto.services.utils.metrics.instruments.gauge.Gauge;
+
  import com.typesafe.config.Config;
+ import com.typesafe.config.ConfigObject;
 
  import akka.actor.ActorRef;
+ import akka.actor.ActorRefWithCell;
  import akka.actor.ActorSystem;
  import akka.dispatch.Envelope;
  import akka.dispatch.MailboxType;
@@ -29,56 +35,64 @@
  import akka.event.LoggingAdapter;
  import scala.Option;
 
- /**
-  * Logs the mailbox size when exceeding the configured limit. It logs at most
-  * once per second when the messages are enqueued or dequeued.
-  *
-  * Configuration:
-  *
-  * <pre>
-  * akka.actor.default-mailbox {
-  *   mailbox-type = akka.contrib.mailbox.LoggingMailboxType
-  *   size-limit = 20
-  * }
-  * </pre>
-  */
- public class LoggingMailboxType implements MailboxType, ProducesMessageQueue<UnboundedMailbox.MessageQueue> {
+ public final class LoggingMailboxType implements MailboxType, ProducesMessageQueue<UnboundedMailbox.MessageQueue> {
+
+     private static final Gauge MAILBOX_SIZE = DittoMetrics.gauge("actor-mailbox-size");
+
      private final Config config;
 
-     public LoggingMailboxType(ActorSystem.Settings settings, Config config) {
+     public LoggingMailboxType(final ActorSystem.Settings settings, final Config config) {
          this.config = config;
      }
 
      @Override
-     public MessageQueue create(Option<ActorRef> owner, Option<ActorSystem> system) {
-         if (owner.isEmpty() || system.isEmpty())
+     public MessageQueue create(final Option<ActorRef> owner, final Option<ActorSystem> system) {
+         if (owner.isEmpty() || system.isEmpty()) {
              throw new IllegalArgumentException("no mailbox owner or system given");
-         int sizeLimit = config.getInt("size-limit");
-         return new LoggingMailbox(owner.get(), system.get(), sizeLimit);
+         }
+
+         final ActorRef mailboxOwner = owner.get();
+
+         final Config mailboxConfig = config.getObject("logging-mailbox").toConfig();
+
+         final int threshold = mailboxConfig.getInt("threshold-for-logging");
+         final long interval = mailboxConfig.getLong("logging-interval");
+
+         if (mailboxOwner.path().toStringWithoutAddress().startsWith("/user")) {
+             return new LoggingMailbox(mailboxOwner, system.get(), threshold, interval);
+         } else {
+             return new UnboundedMailbox.MessageQueue();
+         }
      }
 
-     static class LoggingMailbox implements MessageQueue, akka.dispatch.UnboundedMessageQueueSemantics {
+     static final class LoggingMailbox implements MessageQueue, akka.dispatch.UnboundedMessageQueueSemantics {
 
-         private final Queue<Envelope> queue = new ConcurrentLinkedQueue<Envelope>();
-
-         private final int sizeLimit;
+         private final Queue<Envelope> queue = new ConcurrentLinkedQueue<>();
+         private final int threshold;
          private final LoggingAdapter log;
-         private final long interval = 1000000000L; // 1 s, in nanoseconds
+         private final long interval;
+         private final String ownerActorClassName;
          private final String path;
-         volatile private long logTime = System.nanoTime();
          private final AtomicInteger queueSize = new AtomicInteger();
          private final AtomicInteger dequeueCount = new AtomicInteger();
+         private final Gauge mailboxSizeByActorClassGauge;
+         volatile private long logTime = System.nanoTime();
 
-         LoggingMailbox(ActorRef owner, ActorSystem system, int sizeLimit) {
+         LoggingMailbox(final ActorRef owner, final ActorSystem system, final int threshold, final long interval) {
+
              this.path = owner.path().toString();
-             this.sizeLimit = sizeLimit;
+             this.threshold = threshold;
+             this.interval = interval;
              this.log = Logging.getLogger(system, LoggingMailbox.class);
+             this.ownerActorClassName = getClassOfOwnerActorRef(owner).orElse("unknown-actor-class");
+             this.mailboxSizeByActorClassGauge = MAILBOX_SIZE.tag("actor-class", ownerActorClassName);
          }
 
          @Override
          public Envelope dequeue() {
              Envelope x = queue.poll();
              if (x != null) {
+                 mailboxSizeByActorClassGauge.decrement();
                  int size = queueSize.decrementAndGet();
                  dequeueCount.incrementAndGet();
                  logSize(size);
@@ -90,17 +104,18 @@
          public void enqueue(ActorRef receiver, Envelope handle) {
              queue.offer(handle);
              int size = queueSize.incrementAndGet();
+             mailboxSizeByActorClassGauge.increment();
              logSize(size);
          }
 
          private void logSize(int size) {
-             if (size >= sizeLimit) {
+             if (size >= threshold) {
                  long now = System.nanoTime();
                  if (now - logTime > interval) {
                      double msgPerSecond = ((double) dequeueCount.get()) / (((double) (now - logTime)) / 1000000000L);
                      logTime = now;
                      dequeueCount.set(0);
-                     log.info("Mailbox size for [{}] is [{}], processing [{}] msg/s", path, size,
+                     log.info("Mailbox size for <{}> is <{}>, processing <{}> msg/s", path, size,
                              String.format("%2.2f", msgPerSecond));
                  }
              }
@@ -121,6 +136,15 @@
              for (Envelope handle : queue) {
                  deadLetters.enqueue(owner, handle);
              }
+         }
+     }
+
+     private static Optional<String> getClassOfOwnerActorRef(ActorRef owner) {
+         if (owner instanceof ActorRefWithCell) {
+             final ActorRefWithCell ownerWithCell = (ActorRefWithCell) owner;
+             return Optional.ofNullable(ownerWithCell.underlying().props().actorClass().getCanonicalName());
+         } else {
+             return Optional.empty();
          }
      }
  }
