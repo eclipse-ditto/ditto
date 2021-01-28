a8f9ea277bbe00ce4dca285de1775940b4b74573
Thomas Jaeckle <thomas.jaeckle@bosch.io>
2021-01-28 11:35:48 +0100

    fixed compile errors regarding signature change of Metadata.of
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch.io>

--- services/thingsearch/persistence/src/main/java/org/eclipse/ditto/services/thingsearch/persistence/write/streaming/TestSearchUpdaterStream.java
+++ services/thingsearch/persistence/src/main/java/org/eclipse/ditto/services/thingsearch/persistence/write/streaming/TestSearchUpdaterStream.java
@@ -100,7 +100,7 @@ public final class TestSearchUpdaterStream {
      */
     public Source<WriteResultAndErrors, NotUsed> delete(final ThingId thingId, final long revision,
             @Nullable final PolicyId policyId, final long policyRevision) {
-        return delete(Metadata.of(thingId, revision, policyId, policyRevision));
+        return delete(Metadata.of(thingId, revision, policyId, policyRevision, null));
     }
 
     /**
