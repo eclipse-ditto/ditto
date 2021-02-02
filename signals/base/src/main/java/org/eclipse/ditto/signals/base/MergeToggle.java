2807c3368c3a4591a420e1ce75b1775bc2291fe3
Dominik Guggemos <dominik.guggemos@bosch.io>
2021-02-02 20:35:08 +0100

    #288 add config for merge things feature toggle
    Signed-off-by: Dominik Guggemos <dominik.guggemos@bosch.io>

--- signals/base/src/main/java/org/eclipse/ditto/signals/base/MergeToggle.java
+++ signals/base/src/main/java/org/eclipse/ditto/signals/base/MergeToggle.java
@@ -13,33 +13,33 @@
 package org.eclipse.ditto.signals.base;
 
 /**
- * Decides based on the system property {@value DITTO_FEATURE_MERGE_THINGS_DISABLED} whether the merge thing feature
- * is disabled and throws an {@link org.eclipse.ditto.signals.base.UnsupportedSignalException} if the property is set
- * to {@code true}.
+ * Decides based on the system property {@value MERGE_THINGS_ENABLED} whether the merge thing feature
+ * is enabled and throws an {@link org.eclipse.ditto.signals.base.UnsupportedSignalException} if the property is set
+ * to {@code false}.
  */
 public final class MergeToggle {
 
     /**
-     * System property name of the property defining if the merge feature is disabled.
+     * System property name of the property defining whether the merge feature is enabled.
      */
-    private static final String DITTO_FEATURE_MERGE_THINGS_DISABLED = "ditto.feature.merge-things.disabled";
+    private static final String MERGE_THINGS_ENABLED = "ditto.devops.feature.merge-things-enabled";
 
     /**
-     * Resolves the system property {@value DITTO_FEATURE_MERGE_THINGS_DISABLED}.
+     * Resolves the system property {@value MERGE_THINGS_ENABLED}.
      */
-    private static final boolean IS_MERGE_THINGS_DISABLED = Boolean.getBoolean(DITTO_FEATURE_MERGE_THINGS_DISABLED);
+    private static final boolean IS_MERGE_THINGS_ENABLED = Boolean.getBoolean(MERGE_THINGS_ENABLED);
 
     private MergeToggle() {}
 
     /**
-     * Checks if the merge feature is enabled based on the system property {@value DITTO_FEATURE_MERGE_THINGS_DISABLED}.
+     * Checks if the merge feature is enabled based on the system property {@value MERGE_THINGS_ENABLED}.
      *
      * @param signal the name of the signal that was supposed to be processed
      * @throws org.eclipse.ditto.signals.base.UnsupportedSignalException if the system property
-     * {@value DITTO_FEATURE_MERGE_THINGS_DISABLED} resolves to {@code true}
+     * {@value MERGE_THINGS_ENABLED} resolves to {@code false}
      */
     public static void checkMergeFeatureEnabled(final String signal) {
-        if (IS_MERGE_THINGS_DISABLED) {
+        if (!IS_MERGE_THINGS_ENABLED) {
             throw UnsupportedSignalException.newBuilder(signal).build();
         }
     }
