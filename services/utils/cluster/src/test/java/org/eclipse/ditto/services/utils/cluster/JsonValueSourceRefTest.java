f5782600b65625eec69323c2ef8f3a5e75ca36cc
Juergen Fickel <juergen.fickel@bosch.io>
2021-02-24 07:50:57 +0100

    Added unit test for checking that serialization with jackson-cbor works for `JsonValueSourceRef`.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- services/utils/cluster/src/test/java/org/eclipse/ditto/services/utils/cluster/JsonValueSourceRefTest.java
+++ services/utils/cluster/src/test/java/org/eclipse/ditto/services/utils/cluster/JsonValueSourceRefTest.java
@@ -19,8 +19,12 @@ import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstance
 import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+import org.eclipse.ditto.json.JsonArray;
+import org.eclipse.ditto.json.JsonObject;
 import org.eclipse.ditto.json.JsonValue;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -28,12 +32,14 @@ import org.junit.Test;
 
 import com.typesafe.config.ConfigFactory;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.stream.Materializer;
 import akka.stream.SourceRef;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.stream.javadsl.StreamRefs;
-import akka.testkit.TestKit;
+import akka.testkit.javadsl.TestKit;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import scala.concurrent.duration.FiniteDuration;
 
@@ -79,23 +85,51 @@ public final class JsonValueSourceRefTest {
 
     @Test
     public void getSourceRefReturnsExpected() {
-        final var jsonValueSourceRef = getSourceRef();
+        final var sourceRef = getSourceRef(List.of(JsonValue.of("Hello"), JsonValue.of(" "), JsonValue.of("Ditto!")));
 
-        final var underTest = JsonValueSourceRef.of(jsonValueSourceRef);
+        final var underTest = JsonValueSourceRef.of(sourceRef);
 
-        assertThat(underTest.getSourceRef()).isEqualTo(jsonValueSourceRef);
+        assertThat(underTest.getSourceRef()).isEqualTo(sourceRef);
     }
 
     @Test
-    public void getSourceReturnsNotNull() {
-        final var underTest = JsonValueSourceRef.of(getSourceRef());
+    public void getSourceReturnsNotNull() throws InterruptedException, ExecutionException, TimeoutException {
+        final var jsonValues = List.of(JsonValue.of("Hello"), JsonValue.of(" "), JsonValue.of("Ditto!"));
+        final var underTest = JsonValueSourceRef.of(getSourceRef(jsonValues));
 
-        assertThat(underTest.getSource()).isNotNull();
+        final var actualJsonValues = materializeSource(underTest.getSource());
+
+        assertThat(actualJsonValues).isEqualTo(jsonValues);
     }
 
-    private static SourceRef<JsonValue> getSourceRef() {
-        final var source = Source.from(List.of(JsonValue.of("Hello"), JsonValue.of(" "), JsonValue.of("Ditto!")));
+    @Test
+    public void serializationWorks() throws InterruptedException, ExecutionException, TimeoutException {
+        final var jsonValues = List.of(JsonValue.of("Hello!"), JsonObject.empty(), JsonArray.of(1, 2, true));
+        final var underTest = JsonValueSourceRef.of(getSourceRef(jsonValues));
+        final var messageReceiver = new TestKit(actorSystem);
+        final var messageSender = new TestKit(actorSystem);
+        final var messageReceiverRef = messageReceiver.getRef();
+
+        messageReceiverRef.tell(underTest, messageSender.getRef());
+
+        final var deserializedJsonValueSourceRef = messageReceiver.expectMsgClass(underTest.getClass());
+
+        final var deserializedJsonValues = materializeSource(deserializedJsonValueSourceRef.getSource());
+
+        assertThat(deserializedJsonValues).isEqualTo(jsonValues);
+    }
+
+    private static SourceRef<JsonValue> getSourceRef(final Iterable<JsonValue> jsonValues) {
+        final var source = Source.from(jsonValues);
         return source.runWith(StreamRefs.sourceRef(), Materializer.apply(actorSystem));
+    }
+
+    private static List<JsonValue> materializeSource(final Source<JsonValue, NotUsed> source)
+            throws InterruptedException, ExecutionException, TimeoutException {
+
+        final var completionStage = source.runWith(Sink.seq(), actorSystem);
+        final var completableFuture = completionStage.toCompletableFuture();
+        return completableFuture.get(3, TimeUnit.SECONDS);
     }
 
 }
