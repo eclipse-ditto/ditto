d0ef67f05173320327e82dd0fcb026db0682d381
Stefan Maute <stefan.maute@bosch.io>
2021-06-30 13:48:20 +0200

    add error message into GatewayInternalErrorException;
    Signed-off-by: Stefan Maute <stefan.maute@bosch.io>

--- thingsearch/service/src/main/java/org/eclipse/ditto/thingsearch/service/starter/actors/SearchActor.java
+++ thingsearch/service/src/main/java/org/eclipse/ditto/thingsearch/service/starter/actors/SearchActor.java
@@ -39,7 +39,6 @@ import org.eclipse.ditto.things.model.ThingId;
 import org.eclipse.ditto.thingsearch.api.commands.sudo.SudoCountThings;
 import org.eclipse.ditto.thingsearch.api.commands.sudo.SudoRetrieveNamespaceReport;
 import org.eclipse.ditto.thingsearch.model.SearchModelFactory;
-import org.eclipse.ditto.thingsearch.model.SearchResult;
 import org.eclipse.ditto.thingsearch.model.signals.commands.ThingSearchCommand;
 import org.eclipse.ditto.thingsearch.model.signals.commands.query.CountThings;
 import org.eclipse.ditto.thingsearch.model.signals.commands.query.CountThingsResponse;
@@ -139,7 +138,7 @@ public final class SearchActor extends AbstractActor {
     }
 
     private void namespaceReport(final SudoRetrieveNamespaceReport namespaceReport) {
-        final DittoHeaders dittoHeaders = namespaceReport.getDittoHeaders();
+        final var dittoHeaders = namespaceReport.getDittoHeaders();
         log.withCorrelationId(dittoHeaders)
                 .info("Processing SudoRetrieveNamespaceReport command: {}", namespaceReport);
 
@@ -159,17 +158,13 @@ public final class SearchActor extends AbstractActor {
     private <T extends Command> void executeCount(final T countCommand,
             final Function<T, Query> queryParseFunction,
             final boolean isSudo) {
-        final DittoHeaders dittoHeaders = countCommand.getDittoHeaders();
+        final var dittoHeaders = countCommand.getDittoHeaders();
         log.withCorrelationId(dittoHeaders)
                 .info("Processing CountThings command: {}", countCommand);
         final JsonSchemaVersion version = countCommand.getImplementedSchemaVersion();
-
-        final String queryType = "count";
-
+        final var queryType = "count";
         final StartedTimer countTimer = startNewTimer(version, queryType);
-
         final StartedTimer queryParsingTimer = countTimer.startNewSegment(QUERY_PARSING_SEGMENT_NAME);
-
         final ActorRef sender = getSender();
 
         final Source<Object, ?> replySource = createQuerySource(queryParseFunction, countCommand)
@@ -199,18 +194,20 @@ public final class SearchActor extends AbstractActor {
     private void stream(final StreamThings streamThings) {
         final ThreadSafeDittoLoggingAdapter l = log.withCorrelationId(streamThings);
         l.info("Processing StreamThings command: {}", streamThings);
+
         final JsonSchemaVersion version = streamThings.getImplementedSchemaVersion();
-        final String queryType = "query"; // same as queryThings
+        final var queryType = "query"; // same as queryThings
         final StartedTimer searchTimer = startNewTimer(version, queryType);
         final StartedTimer queryParsingTimer = searchTimer.startNewSegment(QUERY_PARSING_SEGMENT_NAME);
         final ActorRef sender = getSender();
         final Set<String> namespaces = streamThings.getNamespaces().orElse(null);
+
         final Source<Optional<ThingsSearchCursor>, NotUsed> cursorSource =
                 ThingsSearchCursor.extractCursor(streamThings);
         final Source<SourceRef<String>, NotUsed> sourceRefSource = cursorSource.flatMapConcat(cursor -> {
             cursor.ifPresent(c -> c.logCursorCorrelationId(l));
             return createQuerySource(queryParser::parse, streamThings).map(parsedQuery -> {
-                final Query query = ThingsSearchCursor.adjust(cursor, parsedQuery, queryParser.getCriteriaFactory());
+                final var query = ThingsSearchCursor.adjust(cursor, parsedQuery, queryParser.getCriteriaFactory());
                 stopTimer(queryParsingTimer);
                 searchTimer.startNewSegment(DATABASE_ACCESS_SEGMENT_NAME); // segment stopped by stopTimerAndHandleError
                 final List<String> subjectIds =
@@ -230,12 +227,11 @@ public final class SearchActor extends AbstractActor {
     private void query(final QueryThings queryThings) {
         final ThreadSafeDittoLoggingAdapter l = log.withCorrelationId(queryThings);
         l.debug("Starting to process QueryThings command: {}", queryThings);
-        final JsonSchemaVersion version = queryThings.getImplementedSchemaVersion();
 
-        final String queryType = "query";
+        final JsonSchemaVersion version = queryThings.getImplementedSchemaVersion();
+        final var queryType = "query";
         final StartedTimer searchTimer = startNewTimer(version, queryType);
         final StartedTimer queryParsingTimer = searchTimer.startNewSegment(QUERY_PARSING_SEGMENT_NAME);
-
         final ActorRef sender = getSender();
         final Set<String> namespaces = queryThings.getNamespaces().orElse(null);
 
@@ -245,11 +241,11 @@ public final class SearchActor extends AbstractActor {
         final Source<Object, ?> replySource = cursorSource.flatMapConcat(cursor -> {
             cursor.ifPresent(c -> c.logCursorCorrelationId(l));
             final QueryThings command = ThingsSearchCursor.adjust(cursor, queryThings);
-            final DittoHeaders dittoHeaders = command.getDittoHeaders();
+            final var dittoHeaders = command.getDittoHeaders();
             l.info("Processing QueryThings command: {}", queryThings);
             return createQuerySource(queryParser::parse, command)
                     .flatMapConcat(parsedQuery -> {
-                        final Query query =
+                        final var query =
                                 ThingsSearchCursor.adjust(cursor, parsedQuery, queryParser.getCriteriaFactory());
 
                         stopTimer(queryParsingTimer);
@@ -315,6 +311,7 @@ public final class SearchActor extends AbstractActor {
             log.error(error, "SearchActor failed to execute <{}>", trigger);
             return GatewayInternalErrorException.newBuilder()
                     .dittoHeaders(trigger.getDittoHeaders())
+                    .message(error.getClass() + ": " + error.getMessage())
                     .cause(t)
                     .build();
         });
@@ -324,7 +321,7 @@ public final class SearchActor extends AbstractActor {
             @Nullable ThingsSearchCursor cursor,
             final ResultList<ThingId> thingIds) {
 
-        final DittoHeaders dittoHeaders = queryThings.getDittoHeaders();
+        final var dittoHeaders = queryThings.getDittoHeaders();
         if (thingIds.isEmpty()) {
             return QueryThingsResponse.of(SearchModelFactory.emptySearchResult(), dittoHeaders);
         } else {
@@ -336,8 +333,8 @@ public final class SearchActor extends AbstractActor {
                             .build()
                     )
                     .collect(JsonCollectors.valuesToArray());
-            final SearchResult searchResults = SearchModelFactory.newSearchResult(items, thingIds.nextPageOffset());
-            final SearchResult processedResults =
+            final var searchResults = SearchModelFactory.newSearchResult(items, thingIds.nextPageOffset());
+            final var processedResults =
                     ThingsSearchCursor.processSearchResult(queryThings, cursor, searchResults, thingIds);
 
             return QueryThingsResponse.of(processedResults, dittoHeaders);
