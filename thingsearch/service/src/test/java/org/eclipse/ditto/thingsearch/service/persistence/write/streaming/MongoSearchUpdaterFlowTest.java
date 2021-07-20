e68079881de21dbaa9399eaafa39d0d982d09a99
Yufei Cai <yufei.cai@bosch.io>
2021-07-20 16:37:05 +0200

    Fix that configured persistence parallelism had no effect on search updater throughput.
    Signed-off-by: Yufei Cai <yufei.cai@bosch.io>

--- thingsearch/service/src/test/java/org/eclipse/ditto/thingsearch/service/persistence/write/streaming/MongoSearchUpdaterFlowTest.java
+++ thingsearch/service/src/test/java/org/eclipse/ditto/thingsearch/service/persistence/write/streaming/MongoSearchUpdaterFlowTest.java
@@ -13,20 +13,28 @@
 package org.eclipse.ditto.thingsearch.service.persistence.write.streaming;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
-import javax.annotation.Nullable;
-
 import org.bson.Document;
+import org.eclipse.ditto.policies.model.PolicyId;
+import org.eclipse.ditto.things.model.ThingId;
 import org.eclipse.ditto.thingsearch.service.common.config.DefaultPersistenceStreamConfig;
 import org.eclipse.ditto.thingsearch.service.persistence.write.model.AbstractWriteModel;
+import org.eclipse.ditto.thingsearch.service.persistence.write.model.Metadata;
+import org.eclipse.ditto.thingsearch.service.persistence.write.model.ThingWriteModel;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.reactivestreams.Publisher;
@@ -41,9 +49,15 @@ import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 import com.typesafe.config.ConfigFactory;
 
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.stream.DelayOverflowStrategy;
+import akka.stream.Materializer;
 import akka.stream.OverflowStrategy;
+import akka.stream.SystemMaterializer;
+import akka.stream.javadsl.AsPublisher;
+import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.RestartSink;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
@@ -54,19 +68,13 @@ import akka.testkit.javadsl.TestKit;
  */
 public final class MongoSearchUpdaterFlowTest {
 
-    @Nullable
-    private ActorSystem actorSystem;
-
-    @Before
-    public void startActorSystem() {
-        actorSystem = ActorSystem.create();
-    }
+    private final ActorSystem actorSystem =
+            ActorSystem.create(UUID.randomUUID().toString(), ConfigFactory.load("test"));
+    private final Materializer materializer = SystemMaterializer.get(actorSystem).materializer();
 
     @After
     public void shutdownActorSystem() {
-        if (actorSystem != null) {
-            TestKit.shutdownActorSystem(actorSystem);
-        }
+        TestKit.shutdownActorSystem(actorSystem);
     }
 
     @Test
@@ -83,6 +91,70 @@ public final class MongoSearchUpdaterFlowTest {
         testStreamRestart(new FakeMongoExceptionSupplier());
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testThroughput() throws Throwable {
+        new TestKit(actorSystem) {{
+
+            // GIVEN: Persistence has high latency
+
+            final var latency = Duration.ofSeconds(1);
+            final MongoDatabase db = Mockito.mock(MongoDatabase.class);
+            final MongoCollection<Document> collection = Mockito.mock(MongoCollection.class);
+            Mockito.when(db.getCollection(any())).thenReturn(collection);
+            doAnswer(inv -> {
+                final var size = inv.<List<?>>getArgument(0).size();
+                final BulkWriteResult result = BulkWriteResult.acknowledged(0, size, 0, size, List.of(), List.of());
+                return Source.single(result)
+                        .delay(latency, DelayOverflowStrategy.backpressure())
+                        .runWith(Sink.asPublisher(AsPublisher.WITHOUT_FANOUT), actorSystem);
+            }).when(collection).bulkWrite(any(), any(BulkWriteOptions.class));
+
+            final MongoSearchUpdaterFlow flow = MongoSearchUpdaterFlow.of(db,
+                    DefaultPersistenceStreamConfig.of(ConfigFactory.empty()));
+
+            // WHEN: 25 updates go through 32 parallel streams
+
+            final int numberOfChanges = 25;
+            final CountDownLatch latch = new CountDownLatch(numberOfChanges);
+
+            final Sink<Source<AbstractWriteModel, NotUsed>, CompletionStage<Done>> sink =
+                    flow.start(false, 32, 1)
+                            .map(writeResultAndErrors -> {
+                                latch.countDown();
+                                return writeResultAndErrors;
+                            })
+                            .toMat(Sink.ignore(), Keep.right());
+
+            final Metadata metadata = Metadata.of(ThingId.of("thing:id"), 1L, PolicyId.of("policy:id"), 1L, null);
+            final AbstractWriteModel abstractWriteModel = ThingWriteModel.of(metadata, new Document());
+            final Thread testRunnerThread = Thread.currentThread();
+            final AtomicReference<Throwable> errorBox = new AtomicReference<>();
+            Source.repeat(Source.single(abstractWriteModel))
+                    .take(numberOfChanges)
+                    .runWith(Objects.requireNonNull(sink), actorSystem)
+                    .exceptionally(error -> {
+                        errorBox.set(error);
+                        testRunnerThread.interrupt();
+                        return null;
+                    });
+
+            // THEN: updates complete quickly
+
+            try {
+                final var result = latch.await(5L, TimeUnit.SECONDS);
+                assertThat(latch.getCount()).isZero();
+                assertThat(result).isTrue();
+            } catch (final InterruptedException e) {
+                if (errorBox.get() != null) {
+                    throw errorBox.get();
+                } else {
+                    throw e;
+                }
+            }
+        }};
+    }
+
     @SuppressWarnings("unchecked")
     private void testStreamRestart(final Supplier<Throwable> errorSupplier) throws Exception {
 
@@ -93,8 +165,8 @@ public final class MongoSearchUpdaterFlowTest {
             final MongoDatabase db = Mockito.mock(MongoDatabase.class);
             final MongoCollection<Document> collection = Mockito.mock(MongoCollection.class);
             final Publisher<BulkWriteResult> publisher = s -> s.onError(errorSupplier.get());
-            Mockito.when(db.getCollection(Mockito.any())).thenReturn(collection);
-            Mockito.when(collection.bulkWrite(Mockito.any(), Mockito.any(BulkWriteOptions.class)))
+            Mockito.when(db.getCollection(any())).thenReturn(collection);
+            Mockito.when(collection.bulkWrite(any(), any(BulkWriteOptions.class)))
                     .thenReturn(publisher);
 
             // GIVEN: MongoSearchUpdaterFlow is wrapped inside a RestartSink
