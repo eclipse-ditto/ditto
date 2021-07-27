b393e0bb21ff8514a2e820920c05dab805bbdb1c
Yufei Cai <yufei.cai@bosch.io>
2021-07-27 17:44:54 +0200

    Do not publish SubjectDeletedAnnouncement for updated subjects.
    Signed-off-by: Yufei Cai <yufei.cai@bosch.io>

--- policies/service/src/main/java/org/eclipse/ditto/policies/service/persistence/actors/announcements/PolicyAnnouncementManager.java
+++ policies/service/src/main/java/org/eclipse/ditto/policies/service/persistence/actors/announcements/PolicyAnnouncementManager.java
@@ -27,12 +27,14 @@ import org.eclipse.ditto.policies.model.PolicyEntry;
 import org.eclipse.ditto.policies.model.PolicyId;
 import org.eclipse.ditto.policies.model.PolicyLifecycle;
 import org.eclipse.ditto.policies.model.Subject;
+import org.eclipse.ditto.policies.model.SubjectId;
 import org.eclipse.ditto.policies.model.Subjects;
 import org.eclipse.ditto.policies.model.signals.announcements.PolicyAnnouncement;
 import org.eclipse.ditto.policies.service.common.config.PolicyAnnouncementConfig;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.PoisonPill;
 import akka.actor.Props;
 import akka.actor.Terminated;
 import akka.japi.pf.ReceiveBuilder;
@@ -48,6 +50,7 @@ public final class PolicyAnnouncementManager extends AbstractActor {
     private final DistributedPub<PolicyAnnouncement<?>> policyAnnouncementPub;
     private final Map<Subject, ActorRef> subjectExpiryActors;
     private final Map<ActorRef, Subject> activeSubjects;
+    private final Map<SubjectId, Integer> activeSubjectIds;
     private final ActorRef commandForwarder;
     private final PolicyAnnouncementConfig config;
 
@@ -61,6 +64,7 @@ public final class PolicyAnnouncementManager extends AbstractActor {
         this.commandForwarder = commandForwarder;
         this.subjectExpiryActors = new HashMap<>();
         this.activeSubjects = new HashMap<>();
+        this.activeSubjectIds = new HashMap<>();
         this.config = config;
     }
 
@@ -99,6 +103,7 @@ public final class PolicyAnnouncementManager extends AbstractActor {
             startChild(newSubject);
         }
         for (final var deletedSubject : deletedSubjects) {
+            // precondition: child actors have started for new subjects so that modified subjects can be recognized
             sendSubjectDeleted(deletedSubject);
         }
     }
@@ -112,6 +117,7 @@ public final class PolicyAnnouncementManager extends AbstractActor {
         getContext().watch(child);
         subjectExpiryActors.put(subject, child);
         activeSubjects.put(child, subject);
+        addActiveSubjectId(subject);
     }
 
     private void onChildTerminated(final Terminated terminated) {
@@ -119,6 +125,7 @@ public final class PolicyAnnouncementManager extends AbstractActor {
         final var removedSubject = activeSubjects.remove(terminatedActor);
         if (removedSubject != null) {
             final var child = subjectExpiryActors.remove(removedSubject);
+            removeActiveSubjectId(removedSubject);
             log.debug("OnChildTerminated: Removed terminated child <{}>", child);
         } else {
             log.debug("OnChildTerminated: Child not found: <{}>", terminatedActor);
@@ -128,9 +135,27 @@ public final class PolicyAnnouncementManager extends AbstractActor {
     private void sendSubjectDeleted(final Subject subject) {
         final var child = subjectExpiryActors.get(subject);
         if (child != null) {
-            child.tell(SubjectExpiryActor.Message.SUBJECT_DELETED, ActorRef.noSender());
+            notifyChildOfSubjectDeletion(subject, child);
         } else {
             log.error("Attempting to notify nonexistent child for deleted subject <{}>", subject);
+        }
+    }
+
+    /**
+     * Notify a child whose subject was deleted from the policy.
+     *
+     * @param subject the subject.
+     * @param child the recipient.
+     */
+    private void notifyChildOfSubjectDeletion(final Subject subject, final ActorRef child) {
+        final var activeSubjectIdCount = activeSubjectIds.getOrDefault(subject.getId(), 0);
+        if (activeSubjectIdCount >= 2) {
+            // another actor took over the responsibility of child; terminate it.
+            log.debug("Terminating child <{}> of updated subject <{}>", child, subject);
+            child.tell(PoisonPill.getInstance(), ActorRef.noSender());
+        } else {
+            log.debug("Notifying child <{}> of deleted subject <{}>", child, subject);
+            child.tell(SubjectExpiryActor.Message.SUBJECT_DELETED, ActorRef.noSender());
         }
     }
 
@@ -144,6 +169,26 @@ public final class PolicyAnnouncementManager extends AbstractActor {
         } else {
             return Set.of();
         }
+    }
+
+    private void addActiveSubjectId(final Subject subject) {
+        activeSubjectIds.compute(subject.getId(), (k, count) -> {
+            if (count == null) {
+                return 1;
+            } else {
+                return count + 1;
+            }
+        });
+    }
+
+    private void removeActiveSubjectId(final Subject subject) {
+        activeSubjectIds.computeIfPresent(subject.getId(), (k, count) -> {
+            if (count <= 1) {
+                return null;
+            } else {
+                return count - 1;
+            }
+        });
     }
 
     private static List<Subject> calculateDifference(final Set<Subject> minuend, final Set<Subject> subtrahend) {
