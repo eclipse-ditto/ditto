8deccb838813bba67d8943345e9bb56bf7fa6b1e
Thomas Jaeckle <thomas.jaeckle@bosch.io>
2021-08-27 16:39:38 +0200

    made DefaultConnectionConfig public again
    Signed-off-by: Thomas Jaeckle <thomas.jaeckle@bosch.io>

--- connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/config/DefaultConnectionConfig.java
+++ connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/config/DefaultConnectionConfig.java
@@ -34,7 +34,7 @@ import com.typesafe.config.Config;
  * This class implements {@link ConnectionConfig} for Ditto's Connectivity service.
  */
 @Immutable
-final class DefaultConnectionConfig implements ConnectionConfig {
+public final class DefaultConnectionConfig implements ConnectionConfig {
 
     private static final String CONFIG_PATH = "connection";
 
