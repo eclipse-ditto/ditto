b1b6050798dd8e586cfdf6af1303d1a7981b1058
Juergen Fickel <juergen.fickel@bosch.io>
2021-10-13 10:58:10 +0200

    Issue #559: Validate channel header immediately on setting in `DittoHeadersBuilder`.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- base/model/src/main/java/org/eclipse/ditto/base/model/headers/DittoHeadersBuilder.java
+++ base/model/src/main/java/org/eclipse/ditto/base/model/headers/DittoHeadersBuilder.java
@@ -98,10 +98,14 @@ public interface DittoHeadersBuilder<B extends DittoHeadersBuilder<B, R>, R exte
 
     /**
      * Sets the specified String as channel of the Signal/Exception.
+     * Allowed values are {@value "twin"} and {@value "live"}.
+     * If omitted, {@value "twin"} is used as default.
      *
      * @param channel the channel of the Signal/Exception to be set.
+     * {@code null} removes an already set channel header.
      * @return this builder for Method Chaining.
-     * @throws IllegalArgumentException if {@code channel} is empty.
+     * @throws org.eclipse.ditto.base.model.exceptions.DittoHeaderInvalidException if {@code channel} after trimming and
+     * converting to lower case does not match {@value "twin"} or {@value "live"}.
      */
     B channel(@Nullable CharSequence channel);
 
