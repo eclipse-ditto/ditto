7620c8c112b17facf5befc87da9c81907667e799
Juergen Fickel <juergen.fickel@bosch.io>
2022-01-19 13:29:46 +0100

    Simplified serialization test a bit.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- connectivity/service/src/test/java/org/eclipse/ditto/connectivity/service/messaging/ClientActorRefsTest.java
+++ connectivity/service/src/test/java/org/eclipse/ditto/connectivity/service/messaging/ClientActorRefsTest.java
@@ -12,41 +12,33 @@
  */
 package org.eclipse.ditto.connectivity.service.messaging;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.eclipse.ditto.internal.utils.akka.ActorSystemResource;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.typesafe.config.ConfigFactory;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.testkit.javadsl.TestKit;
-
+/**
+ * Unit test for {@link ClientActorRefs}.
+ */
 public final class ClientActorRefsTest {
 
     @ClassRule
-    public static ActorSystemResource actorSystemResource =
+    public static final ActorSystemResource ACTOR_SYSTEM_RESOURCE =
             ActorSystemResource.newInstance(ConfigFactory.load("client-actor-refs-test"));
 
     @Test
     public void serializationWorks() {
-        final ActorSystem actorSystem = actorSystemResource.getActorSystem();
-        final ActorRef ref1 = actorSystemResource.newTestProbe().ref();
-        final ActorRef ref2 = actorSystemResource.newTestProbe().ref();
         final var underTest = ClientActorRefs.empty();
-        underTest.add(ref1);
-        underTest.add(ref2);
-        final var messageReceiver = new TestKit(actorSystem);
-        final var messageSender = new TestKit(actorSystem);
+        underTest.add(ACTOR_SYSTEM_RESOURCE.newTestProbe().ref());
+        underTest.add(ACTOR_SYSTEM_RESOURCE.newTestProbe().ref());
+        final var messageReceiver = ACTOR_SYSTEM_RESOURCE.newTestKit();
+        final var messageSender = ACTOR_SYSTEM_RESOURCE.newTestKit();
         final var messageReceiverRef = messageReceiver.getRef();
 
         messageReceiverRef.tell(underTest, messageSender.getRef());
 
-        final var receivedClientActorRefs = messageReceiver.expectMsgClass(underTest.getClass());
-
-        assertThat(receivedClientActorRefs).isEqualTo(underTest);
+        messageReceiver.expectMsg(underTest);
     }
 
 }
