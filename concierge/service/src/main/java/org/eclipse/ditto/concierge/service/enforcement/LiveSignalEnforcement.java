a9b381effb589c0503b7a3dce53c57fb8da5b436
David Schwilk <david.schwilk@bosch.io>
2022-03-07 13:15:03 +0100

    Adjust logging/ add test
    Signed-off-by: David Schwilk <david.schwilk@bosch.io>

--- concierge/service/src/main/java/org/eclipse/ditto/concierge/service/enforcement/LiveSignalEnforcement.java
+++ concierge/service/src/main/java/org/eclipse/ditto/concierge/service/enforcement/LiveSignalEnforcement.java
@@ -397,13 +397,16 @@ public final class LiveSignalEnforcement extends AbstractEnforcementWithAsk<Sign
             final DistributedPub<T> pub) {
 
         // using pub/sub to publish the command to any interested parties (e.g. a Websocket):
-        log(signal).debug("Publish message to pub-sub: <{}>", signal);
 
         if (enforcementConfig.shouldDispatchGlobally(signal)) {
             return responseReceiverCache.insertResponseReceiverConflictFree(signal,
                     newSignal -> sender(),
-                    (newSignal, receiver) -> publishSignal(newSignal, ackExtractor, pub));
+                    (newSignal, receiver) -> {
+                        log(newSignal).debug("Publish message to pub-sub: <{}>", newSignal);
+                        return publishSignal(newSignal, ackExtractor, pub);
+                    });
         } else {
+            log(signal).debug("Publish message to pub-sub: <{}>", signal);
             return CompletableFuture.completedStage(publishSignal(signal, ackExtractor, pub));
         }
     }
