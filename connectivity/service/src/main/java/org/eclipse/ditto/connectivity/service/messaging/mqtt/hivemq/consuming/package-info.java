d97f274289a7c9eb6cc7cc9c2135e22e715fef1a
Juergen Fickel <juergen.fickel@bosch.io>
2022-04-14 11:29:53 +0200

    Added a package for reactively consuming MQTT messages. Also added a package for abstracting MQTT Publish messages from their protocol version. This abstraction avoids split processing logic which makes it easier to understand what is going on.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

---
+++ connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/messaging/mqtt/hivemq/consuming/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+/**
+ * This package provides the means to consume and process the MQTT Publish messages that are received after subscribing
+ * to MQTT topics.
+ */
+@org.eclipse.ditto.utils.jsr305.annotations.AllParametersAndReturnValuesAreNonnullByDefault
+package org.eclipse.ditto.connectivity.service.messaging.mqtt.hivemq.consuming;
