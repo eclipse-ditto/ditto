82d6f54d64c46b635b89f391458e96103b8206bf
Stefan Maute <stefan.maute@bosch.io>
2022-06-07 15:21:32 +0200

    adapt FeaturePropertiesDeletedStrategyTest and FeatureDesiredPropertiesDeletedStrategyTest because metadata for delete events in now also deleted;
    Signed-off-by: Stefan Maute <stefan.maute@bosch.io>

--- things/service/src/test/java/org/eclipse/ditto/things/service/persistence/actors/strategies/events/FeatureDesiredPropertiesDeletedStrategyTest.java
+++ things/service/src/test/java/org/eclipse/ditto/things/service/persistence/actors/strategies/events/FeatureDesiredPropertiesDeletedStrategyTest.java
@@ -18,13 +18,12 @@ import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable
 
 import java.time.Instant;
 
-import org.eclipse.ditto.json.JsonObject;
-import org.eclipse.ditto.json.JsonPointer;
 import org.eclipse.ditto.base.model.entity.metadata.Metadata;
 import org.eclipse.ditto.base.model.headers.DittoHeaders;
+import org.eclipse.ditto.json.JsonObject;
+import org.eclipse.ditto.json.JsonPointer;
 import org.eclipse.ditto.things.model.Thing;
 import org.eclipse.ditto.things.model.signals.events.FeatureDesiredPropertiesDeleted;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -68,8 +67,7 @@ public final class FeatureDesiredPropertiesDeletedStrategyTest extends AbstractS
                 timestamp, DittoHeaders.empty(), null);
 
         final Metadata existingMetadata = Metadata.newMetadata(JsonObject.newBuilder()
-                .set(JsonPointer.of(String.format("features/%s/desiredProperties", FEATURE_ID)),
-                        JsonObject.of("{\"expired\":true}"))
+                .set(JsonPointer.of(String.format("features/%s", FEATURE_ID)), JsonObject.empty())
                 .set(JsonPointer.of("attributes/additives"), JsonObject.of("{\"E104\":true}"))
                 .build());
         final Thing thingWithFeatureWithDesiredProperties = THING.toBuilder()
