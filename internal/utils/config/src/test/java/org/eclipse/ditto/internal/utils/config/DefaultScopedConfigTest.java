a843974afe166e120dd565f7ad2d71a240904745
Juergen Fickel <juergen.fickel@bosch.io>
2022-07-19 11:44:20 +0200

    Streamline behaviour of DefaultScopedConfig in case of an unknown enum value.
    Signed-off-by: Juergen Fickel <juergen.fickel@bosch.io>

--- internal/utils/config/src/test/java/org/eclipse/ditto/internal/utils/config/DefaultScopedConfigTest.java
+++ internal/utils/config/src/test/java/org/eclipse/ditto/internal/utils/config/DefaultScopedConfigTest.java
@@ -21,7 +21,6 @@ import java.util.Collections;
 
 import org.assertj.core.api.JUnitSoftAssertions;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
