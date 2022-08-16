3f2e79ca6a67460d769effa90c3fe3e30c611fb3
Pranshu-G <pranshu.grover18@gmail.com>
2022-08-16 18:12:21 +0530

    validate headers
    Signed-off-by: Pranshu-G <pranshu.grover18@gmail.com>

--- connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/mapping/CloudEventsMapper.java
+++ connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/mapping/CloudEventsMapper.java
@@ -32,135 +32,153 @@ import org.eclipse.ditto.protocol.JsonifiableAdaptable;
 import org.eclipse.ditto.protocol.ProtocolFactory;
 
 /**
- * A message mapper implementation for the Mapping incoming CloudEvents to Ditto Protocol. 
+ * A message mapper implementation for the Mapping incoming CloudEvents to Ditto Protocol.
  */
 @PayloadMapper(
-    alias = {
-      "CloudEvents",
-      // legacy full qualified name
-      "org.eclipse.ditto.connectivity.service.mapping.CloudEventsMapper"
-    })
+        alias = {
+                "CloudEvents",
+                // legacy full qualified name
+                "org.eclipse.ditto.connectivity.service.mapping.CloudEventsMapper"
+        })
 public final class CloudEventsMapper extends AbstractMessageMapper {
 
-  static final String CE_ID = "ce-id";
-  static final String CE_TYPE = "ce-type";
-  static final String CE_SOURCE = "ce-source";
-  static final String CE_TIME = "ce-time";
+    static final String CE_ID = "ce-id";
+    static final String CE_TYPE = "ce-type";
+    static final String CE_SOURCE = "ce-source";
+    static final String CE_SPECVERSION = "ce-specversion";
 
-  static final JsonObject DEFAULT_OPTIONS =
-      JsonObject.newBuilder()
-          .set(
-              MessageMapperConfiguration.CONTENT_TYPE_BLOCKLIST,
-              String.join(
-                  ",",
-                  "application/vnd.eclipse-hono-empty-notification",
-                  "application/vnd.eclipse-hono-device-provisioning-notification",
-                  "application/vnd.eclipse-hono-dc-notification+json",
-                  "application/vnd.eclipse-hono-delivery-failure-notification+json"))
-          .build();
+    static final JsonObject DEFAULT_OPTIONS =
+            JsonObject.newBuilder()
+                    .set(
+                            MessageMapperConfiguration.CONTENT_TYPE_BLOCKLIST,
+                            String.join(
+                                    ",",
+                                    "application/vnd.eclipse-hono-empty-notification",
+                                    "application/vnd.eclipse-hono-device-provisioning-notification",
+                                    "application/vnd.eclipse-hono-dc-notification+json",
+                                    "application/vnd.eclipse-hono-delivery-failure-notification+json"))
+                    .build();
 
-  /** The context representing this mapper */
-  public static final MappingContext CONTEXT =
-      ConnectivityModelFactory.newMappingContextBuilder(
-              CloudEventsMapper.class.getCanonicalName(), DEFAULT_OPTIONS)
-          .build();
+    /**
+     * The context representing this mapper
+     */
+    public static final MappingContext CONTEXT =
+            ConnectivityModelFactory.newMappingContextBuilder(
+                            CloudEventsMapper.class.getCanonicalName(), DEFAULT_OPTIONS)
+                    .build();
 
-  final String SPECVERSION = "specversion";
-  final String ID = "id";
-  final String SOURCE = "source";
-  final String TYPE = "type";
+    final String SPECVERSION = "specversion";
+    final String ID = "id";
+    final String SOURCE = "source";
+    final String TYPE = "type";
 
-  public String base64decoding(final String base64Message) throws UnsupportedEncodingException {
-    byte[] messageByte = Base64.getDecoder().decode(base64Message);
-    String decodedString = new String(messageByte);
-    return decodedString;
-  }
-
-  boolean validatePayload(String payload) {
-    Map<String, Object> incomingMessagePayload = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-    final JsonObject jsonObject = JsonFactory.newObject(payload);
-    List<JsonKey> key = jsonObject.getKeys();
-    for (JsonKey keyIterator : key) {
-      Object value = jsonObject.getValue(keyIterator).orElse(null);
-      if (value != null) {
-        incomingMessagePayload.put(keyIterator.toString(), value);
-      }
+    public String base64decoding(final String base64Message) throws UnsupportedEncodingException {
+        byte[] messageByte = Base64.getDecoder().decode(base64Message);
+        String decodedString = new String(messageByte);
+        return decodedString;
     }
-    if (incomingMessagePayload.get("specversion") == null
-        || incomingMessagePayload.get("id") == null
-        || incomingMessagePayload.get("source") == null
-        || incomingMessagePayload.get("type") == null) {
-      return false;
-    } else {
-      return true;
+
+    boolean validatePayload(String payload) {
+        Map<String, Object> incomingMessagePayload = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        final JsonObject jsonObject = JsonFactory.newObject(payload);
+        List<JsonKey> key = jsonObject.getKeys();
+        for (JsonKey keyIterator : key) {
+            Object value = jsonObject.getValue(keyIterator).orElse(null);
+            if (value != null) {
+                incomingMessagePayload.put(keyIterator.toString(), value);
+            }
+        }
+        if (incomingMessagePayload.get("specversion") == null
+                || incomingMessagePayload.get("id") == null
+                || incomingMessagePayload.get("source") == null
+                || incomingMessagePayload.get("type") == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
 
-  private JsonifiableAdaptable extractData(final String message)
-      throws UnsupportedEncodingException {
-    final JsonObject payloadJson = JsonFactory.newObject(message);
-    if (payloadJson.getValue("data_base64").isPresent()) {
-      String base64Data = payloadJson.getValue("data_base64").orElse(null).asString();
-      String decodedData = base64decoding(base64Data);
+    private JsonifiableAdaptable extractData(final String message)
+            throws UnsupportedEncodingException {
+        final JsonObject payloadJson = JsonFactory.newObject(message);
+        if (payloadJson.getValue("data_base64").isPresent()) {
+            String base64Data = payloadJson.getValue("data_base64").orElse(null).asString();
+            String decodedData = base64decoding(base64Data);
 
-      final JsonifiableAdaptable decodedJsonifiableAdaptable =
-          DittoJsonException.wrapJsonRuntimeException(
-              () ->
-                  ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(decodedData)));
-      return decodedJsonifiableAdaptable;
+            final JsonifiableAdaptable decodedJsonifiableAdaptable =
+                    DittoJsonException.wrapJsonRuntimeException(
+                            () ->
+                                    ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(decodedData)));
+            return decodedJsonifiableAdaptable;
 
-    } else {
-      String data = payloadJson.getValue("data").get().toString();
-      return DittoJsonException.wrapJsonRuntimeException(
-          () -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data)));
+        } else {
+            String data = payloadJson.getValue("data").get().toString();
+            return DittoJsonException.wrapJsonRuntimeException(
+                    () -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data)));
+        }
     }
-  }
 
-  @Override
-  public List<Adaptable> map(final ExternalMessage message) {
-    // extract message as String
-    final String payload = extractPayloadAsString(message);
-    if (validatePayload(payload)) {
-      try {
-        JsonifiableAdaptable adaptable = extractData(payload);
-        return singletonList(ProtocolFactory.newAdaptableBuilder(adaptable).build());
-      } catch (Throwable e) {
-         e.printStackTrace();
-      }
+    public boolean checkHeaders(final ExternalMessage message) {
+        Map<String, String> headers = message.getHeaders();
+        if (headers.get(CE_ID) == null || headers.get(CE_SOURCE) == null || headers.get(CE_TYPE) == null || headers.get(CE_SPECVERSION) == null) {
+            System.out.println("This is not a Binary CloudEvent");
+            return false;
+        } else {
+            System.out.println("This is a Binary CloudEvent");
+            return true;
+        }
+
     }
-    return null;
-  }
 
-  @Override
-  public DittoHeaders getAdditionalInboundHeaders(ExternalMessage message) {
-    return DittoHeaders.empty();
-  }
+    @Override
+    public List<Adaptable> map(final ExternalMessage message) {
+        // extract message as String
+        checkHeaders(message);
+        System.out.println("The headers are " + message.getHeaders());
+        System.out.println("The headers belong to class " + message.getHeaders().getClass());
 
-  @Override
-  public List<ExternalMessage> map(final Adaptable adaptable) {
-    return List.of(
-        ExternalMessageFactory.newExternalMessageBuilder(getExternalDittoHeaders(adaptable))
-            .withTopicPath(adaptable.getTopicPath())
-            .withText(getJsonString(adaptable))
-            .asResponse(isResponse(adaptable))
-            .asError(isError(adaptable))
-            .build());
-  }
+        final String payload = extractPayloadAsString(message);
+        if (validatePayload(payload)) {
+            try {
+                JsonifiableAdaptable adaptable = extractData(payload);
+                return singletonList(ProtocolFactory.newAdaptableBuilder(adaptable).build());
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 
-  private static DittoHeaders getExternalDittoHeaders(final Adaptable adaptable) {
-    return DittoHeaders.newBuilder()
-        .contentType(DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE)
-        .correlationId(adaptable.getDittoHeaders().getCorrelationId().orElse(null))
-        .build();
-  }
+    @Override
+    public DittoHeaders getAdditionalInboundHeaders(ExternalMessage message) {
+        return DittoHeaders.empty();
+    }
 
-  private static String getJsonString(final Adaptable adaptable) {
-    final var jsonifiableAdaptable = ProtocolFactory.wrapAsJsonifiableAdaptable(adaptable);
-    return jsonifiableAdaptable.toJsonString();
-  }
+    @Override
+    public List<ExternalMessage> map(final Adaptable adaptable) {
+        return List.of(
+                ExternalMessageFactory.newExternalMessageBuilder(getExternalDittoHeaders(adaptable))
+                        .withTopicPath(adaptable.getTopicPath())
+                        .withText(getJsonString(adaptable))
+                        .asResponse(isResponse(adaptable))
+                        .asError(isError(adaptable))
+                        .build());
+    }
 
-  @Override
-  public JsonObject getDefaultOptions() {
-    return DEFAULT_OPTIONS;
-  }
+    private static DittoHeaders getExternalDittoHeaders(final Adaptable adaptable) {
+        return DittoHeaders.newBuilder()
+                .contentType(DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE)
+                .correlationId(adaptable.getDittoHeaders().getCorrelationId().orElse(null))
+                .build();
+    }
+
+    private static String getJsonString(final Adaptable adaptable) {
+        final var jsonifiableAdaptable = ProtocolFactory.wrapAsJsonifiableAdaptable(adaptable);
+        return jsonifiableAdaptable.toJsonString();
+    }
+
+    @Override
+    public JsonObject getDefaultOptions() {
+        return DEFAULT_OPTIONS;
+    }
 }
--- connectivity/service/src/test/java/org/eclipse/ditto/connectivity/service/mapping/CloudEventsMapperTest.java
+++ connectivity/service/src/test/java/org/eclipse/ditto/connectivity/service/mapping/CloudEventsMapperTest.java
@@ -11,6 +11,7 @@ package org.eclipse.ditto.connectivity.service.mapping;
  *
  * SPDX-License-Identifier: EPL-2.0
  */
+
 import org.apache.kafka.common.protocol.types.Field;
 import org.eclipse.ditto.base.model.common.DittoConstants;
 import org.eclipse.ditto.base.model.exceptions.DittoJsonException;
@@ -39,134 +40,72 @@ import static org.junit.Assert.assertEquals;
 
 public class CloudEventsMapperTest {
 
-  private static final ThingId THING_ID = ThingId.of("thing:id");
-  private static final ProtocolAdapter ADAPTER = DittoProtocolAdapter.newInstance();
+    private static final ThingId THING_ID = ThingId.of("thing:id");
+    private static final ProtocolAdapter ADAPTER = DittoProtocolAdapter.newInstance();
 
-  String payload =
-      "{\"specversion\": \"1.0\", \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\"}";
+    String payload = "{\"specversion\": \"1.0\", \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\"}";
 
-  String incompletePayload =
-          "{ \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\"}";
-  String testPayload =
-      "{\n"
-          + "      \"specversion\": \"1.0\", \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\",\n"
-          + "      \"data\":{\n"
-          + "      \"topic\":\"my.sensors/sensor01/things/twin/commands/modify\",\n"
-          + "      \"path\":\"/\",\n"
-          + "      \"value\":{\n"
-          + "          \"thingId\": \"my.sensors:sensor01\",\n"
-          + "          \"policyId\": \"my.test:policy\",\n"
-          + "          \"attributes\": {\n"
-          + "              \"manufacturer\": \"Well known sensors producer\",\n"
-          + "                \"serial number\": \"100\", \n"
-          + "                \"location\": \"Ground floor\" },\n"
-          + "                \"features\": {\n"
-          + "                  \"measurements\": \n"
-          + "                   {\"properties\": \n"
-          + "                   {\"temperature\": 100,\n"
-          + "                    \"humidity\": 0}}}}}}";
+    String incompletePayload = "{ \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\"}";
+    String testPayload = "{\n" + "      \"specversion\": \"1.0\", \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\",\n" + "      \"data\":{\n" + "      \"topic\":\"my.sensors/sensor01/things/twin/commands/modify\",\n" + "      \"path\":\"/\",\n" + "      \"value\":{\n" + "          \"thingId\": \"my.sensors:sensor01\",\n" + "          \"policyId\": \"my.test:policy\",\n" + "          \"attributes\": {\n" + "              \"manufacturer\": \"Well known sensors producer\",\n" + "                \"serial number\": \"100\", \n" + "                \"location\": \"Ground floor\" },\n" + "                \"features\": {\n" + "                  \"measurements\": \n" + "                   {\"properties\": \n" + "                   {\"temperature\": 100,\n" + "                    \"humidity\": 0}}}}}}";
 
-  String data =
-      "{\n"
-          + "  \"topic\":\"my.sensors/sensor01/things/twin/commands/modify\",\n"
-          + "  \"path\":\"/\",\n"
-          + "  \"value\":{\n"
-          + "      \"thingId\": \"my.sensors:sensor01\",\n"
-          + "      \"policyId\": \"my.test:policy\",\n"
-          + "      \"attributes\": {\n"
-          + "          \"manufacturer\": \"Well known sensors producer\",\n"
-          + "            \"serial number\": \"100\", \n"
-          + "            \"location\": \"Ground floor\" },\n"
-          + "            \"features\": {\n"
-          + "              \"measurements\": \n"
-          + "               {\"properties\": \n"
-          + "               {\"temperature\": 100,\n"
-          + "                \"humidity\": 0}}}}}";
+    String data = "{\n" + "  \"topic\":\"my.sensors/sensor01/things/twin/commands/modify\",\n" + "  \"path\":\"/\",\n" + "  \"value\":{\n" + "      \"thingId\": \"my.sensors:sensor01\",\n" + "      \"policyId\": \"my.test:policy\",\n" + "      \"attributes\": {\n" + "          \"manufacturer\": \"Well known sensors producer\",\n" + "            \"serial number\": \"100\", \n" + "            \"location\": \"Ground floor\" },\n" + "            \"features\": {\n" + "              \"measurements\": \n" + "               {\"properties\": \n" + "               {\"temperature\": 100,\n" + "                \"humidity\": 0}}}}}";
 
-  String base64payload =
-      "{\n"
-          + "                    \"specversion\": \"1.0\" , \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\",\n"
-          + "                    \"data_base64\":\"ewogICJ0b3BpYyI6Im15LnNlbnNvcnMvc2Vuc29yMDEvdGhpbmdzL3R3aW4vY29tbWFuZHMvbW9kaWZ5IiwKICAicGF0aCI6Ii8iLAogICJ2YWx1ZSI6ewogICAgICAidGhpbmdJZCI6ICJteS5zZW5zb3JzOnNlbnNvcjAxIiwKICAgICAgInBvbGljeUlkIjogIm15LnRlc3Q6cG9saWN5IiwKICAgICAgImF0dHJpYnV0ZXMiOiB7CiAgICAgICAgICAibWFudWZhY3R1cmVyIjogIldlbGwga25vd24gc2Vuc29ycyBwcm9kdWNlciIsCiAgICAgICAgICAgICJzZXJpYWwgbnVtYmVyIjogIjEwMCIsIAogICAgICAgICAgICAibG9jYXRpb24iOiAiR3JvdW5kIGZsb29yIiB9LAogICAgICAgICAgICAiZmVhdHVyZXMiOiB7CiAgICAgICAgICAgICAgIm1lYXN1cmVtZW50cyI6IAogICAgICAgICAgICAgICB7InByb3BlcnRpZXMiOiAKICAgICAgICAgICAgICAgeyJ0ZW1wZXJhdHVyZSI6IDEwMCwKICAgICAgICAgICAgICAgICJodW1pZGl0eSI6IDB9fX19fQ==\"\n"
-          + "                  }";
+    String base64payload = "{\n" + "                    \"specversion\": \"1.0\" , \"id\":\"3212e\", \"source\":\"http:somesite.com\",\"type\":\"com.site.com\",\n" + "\"data_base64\":\"ewogICJ0b3BpYyI6Im15LnNlbnNvcnMvc2Vuc29yMDEvdGhpbmdzL3R3aW4vY29tbWFuZHMvbW9kaWZ5IiwKICAicGF0aCI6Ii8iLAogICJ2YWx1ZSI6ewogICAgICAidGhpbmdJZCI6ICJteS5zZW5zb3JzOnNlbnNvcjAxIiwKICAgICAgInBvbGljeUlkIjogIm15LnRlc3Q6cG9saWN5IiwKICAgICAgImF0dHJpYnV0ZXMiOiB7CiAgICAgICAgICAibWFudWZhY3R1cmVyIjogIldlbGwga25vd24gc2Vuc29ycyBwcm9kdWNlciIsCiAgICAgICAgICAgICJzZXJpYWwgbnVtYmVyIjogIjEwMCIsIAogICAgICAgICAgICAibG9jYXRpb24iOiAiR3JvdW5kIGZsb29yIiB9LAogICAgICAgICAgICAiZmVhdHVyZXMiOiB7CiAgICAgICAgICAgICAgIm1lYXN1cmVtZW50cyI6IAogICAgICAgICAgICAgICB7InByb3BlcnRpZXMiOiAKICAgICAgICAgICAgICAgeyJ0ZW1wZXJhdHVyZSI6IDEwMCwKICAgICAgICAgICAgICAgICJodW1pZGl0eSI6IDB9fX19fQ==\"\n" + "                  }";
 
-  String data_base64 = Base64.getEncoder().encodeToString(data.getBytes());
-  private CloudEventsMapper underTest;
+    String data_base64 = Base64.getEncoder().encodeToString(data.getBytes());
+    private CloudEventsMapper underTest;
 
-  @Before
-  public void setUp() {
-    underTest = new CloudEventsMapper();
-  }
+    @Before
+    public void setUp() {
+        underTest = new CloudEventsMapper();
+    }
 
-  @Test
-  public void textPayloadMessage() {
-    ExternalMessage message =
-        ExternalMessageFactory.newExternalMessageBuilder(
-                Map.of(
-                    ExternalMessage.CONTENT_TYPE_HEADER,
-                    DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE))
-            .withText(testPayload)
-            .build();
+    @Test
+    public void textPayloadMessage() {
+        ExternalMessage textMessage = textMessageBuilder(testPayload);
+        Adaptable expectedAdaptable = DittoJsonException.wrapJsonRuntimeException(() -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data)));
+        List<Adaptable> expectedMap = singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
+        assertEquals(expectedMap, underTest.map(textMessage));
+    }
 
-    Adaptable expectedAdaptable =
-        DittoJsonException.wrapJsonRuntimeException(
-            () -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data)));
-    List<Adaptable> expectedMap =
-        singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
-    assertEquals(expectedMap, underTest.map(message));
-  }
-  @Test
-  public void base64PayloadMessage() {
-    ExternalMessage message =
-        ExternalMessageFactory.newExternalMessageBuilder(
-                Map.of(
-                    ExternalMessage.CONTENT_TYPE_HEADER,
-                    DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE))
-            .withText(base64payload)
-            .build();
-    String base64 = data_base64.replace("\"", "");
-    byte[] decodedBytes = Base64.getDecoder().decode(base64);
-    String decodedString = new String(decodedBytes);
+    @Test
+    public void base64PayloadMessage() {
+        ExternalMessage message = textMessageBuilder(base64payload);
+        String base64 = data_base64.replace("\"", "");
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+        String decodedString = new String(decodedBytes);
 
-    Adaptable expectedAdaptable =
-        DittoJsonException.wrapJsonRuntimeException(
-            () ->
-                ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(decodedString)));
-    List<Adaptable> expectedMap =
-        singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
-    assertEquals(expectedMap, underTest.map(message));
-  }
+        Adaptable expectedAdaptable = DittoJsonException.wrapJsonRuntimeException(() -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(decodedString)));
+        List<Adaptable> expectedMap = singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
+        assertEquals(expectedMap, underTest.map(message));
+    }
 
-  @Test
-  public void bytePayloadMapping(){
-    ExternalMessage byteMessage =
-            ExternalMessageFactory.newExternalMessageBuilder(
-                            Map.of(
-                                    ExternalMessage.CONTENT_TYPE_HEADER,
-                                    DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE))
-                    .withBytes(testPayload.getBytes(StandardCharsets.UTF_8))
-                    .build();
+    @Test
+    public void bytePayloadMapping() {
+        ExternalMessage byteMessage = ExternalMessageFactory.newExternalMessageBuilder(Map.of(ExternalMessage.CONTENT_TYPE_HEADER, DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE)).withBytes(testPayload.getBytes(StandardCharsets.UTF_8)).build();
 
-    Adaptable expectedAdaptable =
-            DittoJsonException.wrapJsonRuntimeException(
-                    () -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data.getBytes(StandardCharsets.UTF_8))));
-    List<Adaptable> expectedMap =
-            singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
-    assertEquals(expectedMap, underTest.map(byteMessage));
-  }
+        Adaptable expectedAdaptable = DittoJsonException.wrapJsonRuntimeException(() -> ProtocolFactory.jsonifiableAdaptableFromJson(JsonFactory.newObject(data.getBytes(StandardCharsets.UTF_8))));
+        List<Adaptable> expectedMap = singletonList(ProtocolFactory.newAdaptableBuilder(expectedAdaptable).build());
+        assertEquals(expectedMap, underTest.map(byteMessage));
+    }
 
-  @Test
-  public void validatePayloadTest() {
-    Boolean expected = true;
-    Boolean actual = underTest.validatePayload(payload);
-    assertEquals(expected, actual);
-  }
+    @Test
+    public void validatePayloadTest() {
+        Boolean expected = true;
+        Boolean actual = underTest.validatePayload(payload);
+        assertEquals(expected, actual);
+    }
 
-  @Test
-  public void failedValidation(){
-    Boolean expected = false;
-    Boolean actual = underTest.validatePayload(incompletePayload);
-    assertEquals(expected,actual);
+    @Test
+    public void failedValidation() {
+        Boolean expected = false;
+        Boolean actual = underTest.validatePayload(incompletePayload);
+        assertEquals(expected, actual);
 
-  }
+    }
 
+    private ExternalMessage textMessageBuilder(String textPayload) {
+        ExternalMessage message = ExternalMessageFactory.newExternalMessageBuilder(Map.of(ExternalMessage.CONTENT_TYPE_HEADER, DittoConstants.DITTO_PROTOCOL_CONTENT_TYPE)).withText(textPayload).build();
+        return message;
+    }
 }
