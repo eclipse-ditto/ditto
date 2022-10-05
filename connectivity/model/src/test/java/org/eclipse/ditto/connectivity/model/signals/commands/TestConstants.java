36e1e13da21d08ba1a79beb6c8f904ea185459a7
Andrey Balarev <andrey.balarev@bosch.io>
2022-10-05 09:52:56 +0300

    Copyright headers fixes
    Signed-off-by: Andrey Balarev <andrey.balarev@bosch.io>

--- connectivity/model/src/test/java/org/eclipse/ditto/connectivity/model/signals/commands/TestConstants.java
+++ connectivity/model/src/test/java/org/eclipse/ditto/connectivity/model/signals/commands/TestConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
