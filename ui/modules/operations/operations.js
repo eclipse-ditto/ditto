71acc960152c1d97385a0f3104ad18f9b28dc521
thfries <thomas.fries0@gmail.com>
2023-03-18 21:14:40 +0100

    UI - Operations Tab: new logger view
    Signed-off-by: thfries <thomas.fries0@gmail.com>

--- ui/modules/operations/operations.js
+++ ui/modules/operations/operations.js
@@ -18,82 +18,65 @@ import * as API from '../api.js';
 import * as Environments from '../environments/environments.js';
 import * as Utils from '../utils.js';
 
-let opsLoggingEditor;
-
-let allLogLevels;
-
 let dom = {
   buttonLoadAllLogLevels: null,
-  tableValidationOpsLogging: null,
-  tbodyOpsLogging: null,
-  crudOpsLoggingJson: null,
+  divLoggers: null,
+  templateLogger: null,
   tabOperations: null,
   collapseOperations: null,
 };
 
 export async function ready() {
   Environments.addChangeListener(onEnvironmentChanged);
-
   Utils.getAllElementsById(dom);
 
-  Utils.addValidatorToTable(dom.tbodyOpsLogging, dom.tableValidationOpsLogging);
-
   dom.tabOperations.onclick = onTabActivated;
-
-  opsLoggingEditor = Utils.createAceEditor('opsLoggingEditor', 'ace/mode/json', true);
-
   dom.buttonLoadAllLogLevels.onclick = loadAllLogLevels;
-  dom.tbodyOpsLogging.addEventListener('click', onLoggingTableClick);
-
-  dom.crudOpsLoggingJson.editDisabled = true;
-  dom.crudOpsLoggingJson.createDisabled = true;
-  dom.crudOpsLoggingJson.deleteDisabled = true;
-
-  dom.crudOpsLoggingJson.addEventListener('onEditToggle', onEditToggle);
-  dom.crudOpsLoggingJson.addEventListener('onUpdateClick', onUpdateLoggingClick);
 }
 
-function onLoggingTableClick(event) {
-  if (event.target && event.target.tagName === 'TD') {
-    if (isSelected(event.target.parentNode.id, event.target.parentNode.logLevel)) {
-      updateLogEditor();
-    } else {
-      updateLogEditor(event.target.parentNode.id, event.target.parentNode.logLevel);
-    }
-  }
-}
-
-function updateLogEditor(logService, logLevel) {
-  if (logService) {
-    dom.crudOpsLoggingJson.idValue = logService;
-    dom.crudOpsLoggingJson.editDisabled = false;
-    opsLoggingEditor.setValue(JSON.stringify(logLevel, null, 2), -1);
-  } else {
-    dom.crudOpsLoggingJson.idValue = null;
-    dom.crudOpsLoggingJson.editDisabled = true;
-    opsLoggingEditor.setValue('');
-  }
-}
-
-let undoLogLevel;
-
-function onEditToggle(event) {
-  opsLoggingEditor.setReadOnly(!event.detail.isEditing);
-  opsLoggingEditor.renderer.setShowGutter(event.detail.isEditing);
-  if (event.detail.isEditing) {
-    undoLogLevel = JSON.parse(opsLoggingEditor.getValue());
-  } else if (event.detail.isCancel) {
-    updateLogEditor(dom.crudOpsLoggingJson.idValue, undoLogLevel);
-  }
-}
-
-function onUpdateLoggingClick() {
-  API.callDittoREST('PUT', '/devops/logging/' + dom.crudOpsLoggingJson.idValue,
-      JSON.parse(opsLoggingEditor.getValue()), null, false, true)
+function onUpdateLoggingClick(service, logConfig) {
+  API.callDittoREST('PUT', '/devops/logging/' + service, logConfig, null, false, true)
       .then(() => {
         loadAllLogLevels();
-        dom.crudOpsLoggingJson.toggleEdit();
       });
+}
+
+function loadAllLogLevels() {
+  API.callDittoREST('GET', '/devops/logging', null, null, false, true)
+      .then((result) => createLoggerView(result))
+      .catch((error) => {
+      });
+}
+
+function createLoggerView(allLogLevels) {
+  dom.divLoggers.innerHTML = '';
+  Object.keys(allLogLevels).sort().forEach((service, i) => {
+    let heading = document.createElement('h6');
+    heading.innerText = service;
+    if (i > 0) {
+      heading.classList.add('mt-3');
+    }
+    dom.divLoggers.append(heading);
+    Object.values(allLogLevels[service])[0].loggerConfigs.forEach((logConfig) => {
+      let row = document.createElement('div');
+      row.attachShadow({mode: 'open'});
+      row.shadowRoot.append(dom.templateLogger.content.cloneNode(true));
+      row.shadowRoot.getElementById('inputLogger').value = logConfig.logger;
+      row.shadowRoot.getElementById(logConfig.level).setAttribute('checked', '');
+      Array.from(row.shadowRoot.querySelectorAll('.btn-check')).forEach(
+          (btn) => {
+            btn.service = service;
+            btn.logger = logConfig.logger;
+            btn.addEventListener('click', (event) => {
+              onUpdateLoggingClick(event.target.service, {
+                logger: event.target.logger,
+                level: event.target.id,
+              });
+            });
+          });
+      dom.divLoggers.append(row);
+    });
+  });
 }
 
 let viewDirty = false;
@@ -107,37 +90,8 @@ function onTabActivated() {
 
 function onEnvironmentChanged() {
   if (dom.collapseOperations.classList.contains('show')) {
-    dom.tbodyOpsLogging.innerHTML = '';
-    updateLogEditor();
     loadAllLogLevels();
   } else {
     viewDirty = true;
   }
 }
-
-function loadAllLogLevels() {
-  API.callDittoREST('GET', '/devops/logging', null, null, false, true)
-      .then((result) => {
-        allLogLevels = result;
-        dom.tbodyOpsLogging.innerHTML = '';
-        Object.keys(allLogLevels).forEach((service) => {
-          Object.values(allLogLevels[service])[0].loggerConfigs.forEach((logConfig) => {
-            const row = Utils.addTableRow(dom.tbodyOpsLogging, service,
-                isSelected(service, logConfig),
-                false,
-                logConfig.level,
-                logConfig.logger);
-            row.logLevel = logConfig;
-          });
-        });
-      })
-      .catch((error) => {
-      });
-}
-
-function isSelected(logService, logConfig) {
-  const selectedService = dom.crudOpsLoggingJson.idValue;
-  const selectedLogLevel = opsLoggingEditor.getValue() ? JSON.parse(opsLoggingEditor.getValue()) : null;
-  return (selectedLogLevel && logService === selectedService && logConfig.level === selectedLogLevel.level && logConfig.logger === selectedLogLevel.logger)
-}
-
