b9d874d41307c2f817f772e2bacb25ea4531360c
thfries <thomas.fries0@gmail.com>
2023-11-13 07:10:58 +0100

    UI - policy imports missing headers in new files
    Signed-off-by: thfries <thomas.fries0@gmail.com>

--- ui/modules/policies/policiesJSON.ts
+++ ui/modules/policies/policiesJSON.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 import * as Utils from '../utils.js';
 import * as API from '../api.js';
 import * as Environments from '../environments/environments.js'
