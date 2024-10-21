88a3615124bb26e136b8e5df7b34ed674a11e64a
Hussein Ahmed <hussein.a.sattar@hotmail.com>
2024-10-21 09:39:18 +0200

    fix missing license

--- policies/model/src/main/java/org/eclipse/ditto/policies/model/signals/commands/query/PolicyCheckPermissionsResponse.java
+++ policies/model/src/main/java/org/eclipse/ditto/policies/model/signals/commands/query/PolicyCheckPermissionsResponse.java
@@ -5,6 +5,10 @@
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
  *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.ditto.policies.model.signals.commands.query;
